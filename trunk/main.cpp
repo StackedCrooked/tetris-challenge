9073e52da5dd1bcaf7d9bfe10f44420115fc0ed9
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2009-06-21 13:45:15 +0000

    Ascii output works now.

--- trunk/main.cpp
+++ trunk/main.cpp
@@ -6,18 +6,47 @@
 
 namespace Tetris
 {
-		
+	void fillCharGrid(const GameStateNode * node, GenericGrid<char> & grid)
+	{
+		GameStateNode::Children::const_iterator it = node->children().begin(), end = node->children().end();
+		for (; it != end; ++it)
+		{
+			const GameStateNode * childNode = it->get();
+			if (!childNode->isDeadEnd())
+			{
+				char id = childNode->lastBlock().charId();
+				size_t row, col;
+				childNode->lastBlockPosition(row, col);
+				for (size_t r = row; r != row + childNode->lastBlock().grid().numRows(); ++r)
+				{
+					for (size_t c = col; c != col + childNode->lastBlock().grid().numColumns(); ++c)
+					{
+						if (childNode->lastBlock().grid().get(r - row, c - col) != NO_BLOCK)
+						{
+							grid.set(r, c, id);
+						}
+					}
+				}
+				fillCharGrid(childNode, grid);
+				return;
+			}
+		}
+	}
+
+
 	void printState(const GameStateNode * inRootNode)
 	{
-		GenericGrid<char> grid(15, 15, ' ');
-
-			//for (size_t rowIdx = 0; rowIdx != inState.grid().numRows(); ++rowIdx)
-		//{
-		//	for (size_t colIdx = 0; colIdx != inState.grid().numColumns(); ++colIdx)
-		//	{
-		//		//std::cout << inState.grid().get(rowIdx, colIdx)
-		//	}
-		//}
+		GenericGrid<char> grid(15, 15, '.');
+		const GameStateNode * node = inRootNode;
+		fillCharGrid(node, grid);
+		for (size_t rowIdx = 0; rowIdx != grid.numRows(); ++rowIdx)
+		{
+			for (size_t colIdx = 0; colIdx != grid.numColumns(); ++colIdx)
+			{
+				std::cout << grid.get(rowIdx, colIdx);
+			}
+			std::cout << std::endl;
+		}
 	}
 
 }
