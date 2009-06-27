7e21eca7fa9926bd4694e129c0c3212deddb9331
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2009-06-27 18:56:30 +0000

    Add CStopWatch files, forgot this.

---
+++ trunk/CStopWatch.h
@@ -0,0 +1,24 @@
+#ifndef CSTOPWATCH_H
+#define CSTOPWATCH_H
+
+#include <windows.h>
+
+typedef struct {
+    LARGE_INTEGER start;
+    LARGE_INTEGER stop;
+} stopWatch;
+
+class CStopWatch {
+
+private:
+	stopWatch timer;
+	LARGE_INTEGER frequency;
+	double LIToSecs( LARGE_INTEGER & L);
+public:
+	CStopWatch();
+	void startTimer( );
+	void stopTimer( );
+	double getElapsedTime();
+};
+
+#endif // CSTOPWATCH_H
