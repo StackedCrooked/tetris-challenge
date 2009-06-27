7ef598bdfa4ab809274cd13cb329166727ee235a
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2009-06-27 18:46:21 +0000

    Use CStopWatch to measure timings.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include "PuzzleSolver.h"
+#include "CStopWatch.h"
 #include "GameState.h"
+#include "PuzzleSolver.h"
 #include <ctime>
 #include <iostream>
 #include <string>
@@ -36,8 +37,8 @@ namespace Tetris
 
 int main()
 {
-	std::cout << "Running..." << std::endl;
-	std::time_t start(time(0));
+	CStopWatch stopWatch;
+	stopWatch.startTimer();
 	Tetris::PuzzleSolver puzzleSolver;
 	int count = 0;
 	int maxDepth = 0;
@@ -46,16 +47,10 @@ int main()
 		puzzleSolver.next();
 		if (puzzleSolver.depth() > maxDepth)
 		{
-			std::cout << "Depth has reached: " << puzzleSolver.depth() << ". Count: " << count << std::endl;
 			maxDepth = puzzleSolver.depth();
-		}
-		if (count % 10000 == 0)
-		{
-			std::cout << "Count: " << count << std::endl;
 		}
 		count++;
 	}
-	std::cout << "Duration: " << time(0) - start << " s" << std::endl;	
 
 	Tetris::GenericGrid<char> grid(15, 15, '.');
 	puzzleSolver.getAsciiFormat(grid);
@@ -66,6 +61,8 @@ int main()
 	Tetris::printListOfMoves(moves, std::cout);
 	
 	
+	stopWatch.stopTimer();
+	std::cout << "Duration: " << stopWatch.getElapsedTime() << "." << std::endl;
 	std::cout << "Press ENTER to quit";
 	getchar();
 	return 0;
