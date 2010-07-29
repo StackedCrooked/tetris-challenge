24f2c05733f99c57fc43ccc340c64d616c36d067
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-07-29 18:10:45 +0000

    - Add folders TetrisCore and Test.

--- trunk/Test/WinMain.cpp
+++ trunk/Test/WinMain.cpp
@@ -1,11 +1,10 @@
 #include "Visualizer.h"
-#include "PuzzleSolver.h"
 
 
 INT_PTR WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    Tetris::PuzzleSolver puzzleSolver;
-    Tetris::Visualizer v(&puzzleSolver);
-    v.show();
+    //Tetris::PuzzleSolver puzzleSolver;
+    //Tetris::Visualizer v(&puzzleSolver);
+    //v.show();
     return 0;
 }
