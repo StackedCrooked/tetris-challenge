152e86bb847a1e8ce116b0e46c3c614fae0b3fae
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-07-29 21:23:49 +0000

    Add Game class.

---
+++ trunk/TetrisCore/include/Game.h
@@ -0,0 +1,33 @@
+#ifndef GAME_H
+#define GAME_H
+
+
+#include "Block.h"
+#include "GameStateNode.h"
+
+
+namespace Tetris
+{
+
+    // This is the model, not the controller
+    class Game
+    {
+    public:
+        Game(int inNumRows, int inNumColumns);
+
+        // Drops the active block one square
+        void tick();
+
+        void commitActiveBlock();
+
+    private:
+        const Block & getNextBlock(int inDepth);
+
+        GameStateNode mRootNode;
+        GameStateNode * mCurrentNode;
+        std::vector<BlockType> mNextBlocks;
+    };
+
+} // namespace Tetris
+
+#endif // GAME_H
---
+++ trunk/TetrisCore/src/Game.cpp
@@ -0,0 +1,36 @@
+#include "Game.h"
+#include "GameState.h"
+
+
+namespace Tetris
+{
+    Game::Game(int inNumRows, int inNumColumns) :
+        mRootNode(new GameState(inNumRows, inNumColumns))
+    {
+        mCurrentNode = &mRootNode;
+    }
+
+
+    void Game::tick()
+    {
+        //mRootNode->tick();
+    }
+
+
+    const Block & getNextBlock(int inDepth)
+    {
+        static Block block(BlockType_J, 0);
+        return block;
+    }
+
+
+    void Game::commitActiveBlock()
+    {
+        //const Block & nextBlock = getNextBlock(mCurrentNode->depth());
+        //ChildPtr child(mRootNode.state().commit(nextBlock).release());
+        //mCurrentNode->children().insert(child);
+    }
+
+
+
+} // namespace Tetris
