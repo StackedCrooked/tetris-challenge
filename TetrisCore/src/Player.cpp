e3a3d79bbc5fc3a1ad21f4891444a5068834e598
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-08-01 03:11:28 +0000

    Add Game::getFutureBlocks. PopulteNode attempts.

--- TetrisCore/src/Player.cpp
+++ TetrisCore/src/Player.cpp
@@ -1,4 +1,5 @@
 #include "Player.h"
+#include "ErrorHandling.h"
 
 
 namespace Tetris
@@ -11,14 +12,43 @@ namespace Tetris
     }
 
 
+    template<class T>
+    std::vector<T> DropFirst(const std::vector<T> & inValues)
+    {
+        std::vector<T> result;
+        for (size_t i = 1; i < inValues.size(); ++i)
+        {
+            result.push_back(inValues[i]);
+        }
+        return result;
+    }
+
+
+    void PopulateNode(GameStateNode & inNode, const std::vector<Block> & inBlocks, const std::vector<int> & inSelectionCounts)
+    {
+        CheckArgument(inBlocks.size() == inSelectionCounts.size(), "PopulateNode got inBlocks.size() != inSelectionCounts.size()");
+        if (inBlocks.empty())
+        {
+            return;
+        }
+
+        inNode.populate(inBlocks.front());
+        for (size_t i = 0; i != inSelectionCounts.size(); ++i)
+        {
+            Children & children = inNode.children();
+            Children::iterator it = children.begin();
+            const int selectionCount = inSelectionCounts[i];
+            for (int j = 0; j < selectionCount && it != children.end(); ++j)
+            {
+                PopulateNode(*(it->get()), DropFirst(inBlocks), DropFirst(inSelectionCounts));
+            }
+        }
+    }
+
+
     void Player::move(const std::vector<int> & inDepths)
     {
-        GameStateNode & node = mGame->currentNode();
-        GameState & state = node.state();
-        
-
-
-        mGame->drop();
+        PopulateNode(mGame->currentNode(), mGame->getFutureBlocks(inDepths.size()), inDepths);
     }
 
 
