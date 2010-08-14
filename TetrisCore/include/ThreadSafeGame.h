de1c62d8b1bc6368af3fe76b5232a75520eab521
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-08-14 06:25:20 +0000

    Add some more documentation.

--- TetrisCore/include/ThreadSafeGame.h
+++ TetrisCore/include/ThreadSafeGame.h
@@ -5,6 +5,7 @@
 #include "Game.h"
 #include <boost/function.hpp>
 #include <boost/thread.hpp>
+#include <boost/shared_ptr.hpp>
 #include <memory>
 
 
@@ -15,6 +16,26 @@ namespace Tetris
 
     /**
      * ThreadSafeGame
+     *
+     * Takes ownership of a Game object and pairs it with a mutex.
+     * The Game object is stored in a shared_ptr so that you can create
+     * multiple copies of the same ThreadSafeGame object.
+     *
+     * Users of this class should always first lock the mutex before
+     * accessing the Game pointer.
+     *
+     * Example usage:
+     *
+     * // Assuming "threadSafeGame" is pointer to a ThreadSafeGame object.
+     * // Create an additional scope to define the scope of the scoped_lock object.
+     * {
+     *     // First lock the game object.
+     *     boost::mutex::scoped_lock lock(threadSafeGame->getMutex());
+     *
+     *     // Then access it.
+     *     Game * game = threadSafeGame->getGame();
+     *     game->move(Direction_Down);
+     * }
      *
      */
     class ThreadSafeGame
@@ -29,11 +50,9 @@ namespace Tetris
         boost::mutex & getMutex() const;
 
     private:
-        boost::scoped_ptr<Game> mGame;
+        boost::shared_ptr<Game> mGame;
         mutable boost::mutex mMutex;
     };
-
-
 
 } // namespace Tetris
 
