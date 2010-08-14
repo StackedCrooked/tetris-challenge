0876d4a00aed03844303400e7f801f862f16a905
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-08-14 04:57:59 +0000

    Implement ThreadSafeGame methods.

--- TetrisCore/include/ThreadSafeGame.h
+++ TetrisCore/include/ThreadSafeGame.h
@@ -25,13 +25,58 @@ namespace Tetris
         // A function that can modify the game object
         typedef boost::function<void(Game*)> Action;
 
-        void doto(const Action & inAction);
+        void doto(const Action & inAction);        
+
+        int numRows() const;
+
+        int numColumns() const;
+
+        void reserveBlocks(size_t inCount);
+
+        const Block & activeBlock() const;
+
+        Block & activeBlock();
+
+        // Includes the currently active block
+        void getFutureBlocks(size_t inCount, BlockTypes & outBlocks) const;
+
+        GameStateNode * currentNode();
+
+        const GameStateNode * currentNode() const;
+
+        void setCurrentNode(GameStateNode * inCurrentNode);
+
+        bool isGameOver() const;
+
+        //
+        // Game commands
+        //
+        bool move(Direction inDirection);
+
+        void rotate();
+
+        void drop();
+
+        //
+        // Navigate the game history and alternative histories.
+        // Experimental.
+        //
+        bool navigateNodeUp();
+
+        bool navigateNodeDown();
+
+        bool navigateNodeLeft();
+
+        bool navigateNodeRight();
 
     private:
         boost::scoped_ptr<Game> mGame;
         mutable boost::mutex mMutex;
     };
 
+
+
 } // namespace Tetris
+
 
 #endif // THREADSAFEGAME_H_INCLUDED
