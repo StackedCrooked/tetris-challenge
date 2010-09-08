bd0bc0b59c14da45a012c4640ade0b2c1dba3ac6
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-09-08 17:51:59 +0000

    Move DivideByTwo function from Grid to Utilities.

--- trunk/TetrisCore/include/Tetris/Grid.h
+++ trunk/TetrisCore/include/Tetris/Grid.h
@@ -11,13 +11,6 @@ namespace Tetris
 
     typedef GenericGrid<BlockType> Grid;
 
-    // Rounded division by 2.
-    // To be used for integral values (int, size_t, ...).
-    template<class T>
-    T DivideByTwo(T inValue)
-    {
-        return (int)(0.5 + ((float)inValue / (float)2.0));
-    }
 
 } // namespace Tetris
 
--- trunk/TetrisCore/include/Tetris/Utilities.h
+++ trunk/TetrisCore/include/Tetris/Utilities.h
@@ -6,7 +6,16 @@
 
 
 namespace Tetris
-{
+{    
+
+    // Rounded division by 2.
+    // To be used for integral values (int, size_t, ...).
+    template<class T>
+    T DivideByTwo(T inValue)
+    {
+        return (int)(0.5 + ((float)inValue / (float)2.0));
+    }
+
 
     // Helper functions to reduce the typing involed when creating auto_ptr objects.
     // For example:
