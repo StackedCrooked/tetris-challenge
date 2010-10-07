9b3304ead280fec0b990982529be47d54ce0cc48
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-10-07 19:18:09 +0000

    Scripts: Improve formatter script.

--- Scripts/AStyle/FormatCode.bat
+++ Scripts/AStyle/FormatCode.bat
@@ -1,10 +1,12 @@
 @echo off
 if "%1" == "" goto error
 
-Scripts\AStyle\AStyle.exe --indent=spaces=4 --brackets=break --indent-switches --indent-namespaces --indent-labels --indent-preprocessor --pad-header --unpad-paren --convert-tabs --align-pointer=middle --suffix=none --lineend=windows --keep-one-line-blocks --keep-one-line-statements --recursive %1
+SET ASTYLE_COMMAND=Scripts\AStyle\AStyle.exe --indent=spaces=4 --brackets=break --indent-switches --indent-namespaces --indent-labels --indent-preprocessor --pad-header --unpad-paren --convert-tabs --align-pointer=middle --suffix=none --lineend=windows --keep-one-line-blocks --keep-one-line-statements --recursive
+%ASTYLE_COMMAND% %1\*.h
+%ASTYLE_COMMAND% %1\*.cpp
 goto :end
 
 :error
-echo Usage: Scripts\AStyle\FormatCode.bat TetrisCore\src\*.cpp
+echo Usage: Scripts\AStyle\FormatCode.bat PATH
 
 :end
