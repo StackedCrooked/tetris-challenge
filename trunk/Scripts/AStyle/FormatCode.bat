4071d121be7417de371fe01ce69b78c9090ca460
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-10-07 18:48:20 +0000

    Add Scripts directory for various build utilities. Add code formatter utility.

---
+++ trunk/Scripts/AStyle/FormatCode.bat
@@ -0,0 +1,10 @@
+@echo off
+if "%1" == "" goto error
+
+Scripts\AStyle\AStyle.exe --indent=spaces=4 --brackets=break --indent-switches --indent-namespaces --indent-labels --indent-preprocessor --pad-header --unpad-paren --convert-tabs --align-pointer=middle --suffix=none --lineend=windows --delete-empty-lines --keep-one-line-blocks --keep-one-line-statements --recursive %1
+goto :end
+
+:error
+echo Usage: Scripts\AStyle\FormatCode.bat TetrisCore\src\*.cpp
+
+:end
