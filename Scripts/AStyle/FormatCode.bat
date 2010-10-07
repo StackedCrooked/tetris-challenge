df368d0800d3405a8ddf723b8b82298f66429a6b
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-10-07 18:58:00 +0000

    Scripts\AStyle: Remove --delete-empty-lines option.

--- Scripts/AStyle/FormatCode.bat
+++ Scripts/AStyle/FormatCode.bat
@@ -1,7 +1,7 @@
 @echo off
 if "%1" == "" goto error
 
-Scripts\AStyle\AStyle.exe --indent=spaces=4 --brackets=break --indent-switches --indent-namespaces --indent-labels --indent-preprocessor --pad-header --unpad-paren --convert-tabs --align-pointer=middle --suffix=none --lineend=windows --delete-empty-lines --keep-one-line-blocks --keep-one-line-statements --recursive %1
+Scripts\AStyle\AStyle.exe --indent=spaces=4 --brackets=break --indent-switches --indent-namespaces --indent-labels --indent-preprocessor --pad-header --unpad-paren --convert-tabs --align-pointer=middle --suffix=none --lineend=windows --keep-one-line-blocks --keep-one-line-statements --recursive %1
 goto :end
 
 :error
