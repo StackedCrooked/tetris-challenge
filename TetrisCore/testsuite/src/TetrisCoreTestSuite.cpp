2a97ee00ddd0936c9dafa528db0b7cf5c52ea2c5
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-10-12 22:21:34 +0000

    Add unit tests for WorkerPool. Fix bug in WorkerPool.

--- TetrisCore/testsuite/src/TetrisCoreTestSuite.cpp
+++ TetrisCore/testsuite/src/TetrisCoreTestSuite.cpp
@@ -8,7 +8,7 @@ CppUnit::Test* TetrisCoreTestSuite::suite()
 	CppUnit::TestSuite* pSuite = new CppUnit::TestSuite("TetrisCoreTestSuite");
 
 	pSuite->addTest(WorkerTest::suite());
-	//pSuite->addTest(WorkerPoolTest::suite());
+	pSuite->addTest(WorkerPoolTest::suite());
 
 	return pSuite;
 }
