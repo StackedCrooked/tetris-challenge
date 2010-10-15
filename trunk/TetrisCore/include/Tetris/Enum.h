3ab3425de46bcdabac22c6918e9107e95efe9eee
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-10-15 22:09:30 +0000

    Tetris/Enum.h: Update documentation.

--- trunk/TetrisCore/include/Tetris/Enum.h
+++ trunk/TetrisCore/include/Tetris/Enum.h
@@ -3,23 +3,31 @@
 
 
 /**
- * DefineEnum can be used to define a (fake) enum.
- * For example:
+ * DefineEnum can be used to define a fake enum. The purpose of this macro
+ * is to enable enums that can be forward declared.
+ *
+ * Example:
  *
  *     DefineEnum(Color)
  *     {
- *       Red,
- *       Green,
- *       Blue
+ *       Color_Red,   // Color_* naming style compensates a bit for the loss of type-safety.
+ *       Color_Green,
+ *       Color_Blue
  *     };
  *
+ *     Color c = Color_Blue;
  *
- * DeclareEnum can be used to create a forward declaration.
- * For example:
+ */
+#define DefineEnum(name) typedef int name; enum
+
+
+/*
+ * DeclareEnum can be used to create a forward declaration of a fake enum.
+ *
+ * Example:
  *
  *    DeclareEnum(Color);
  */
-#define DefineEnum(name) typedef int name; enum
 #define DeclareEnum(name) typedef int name
 
 
