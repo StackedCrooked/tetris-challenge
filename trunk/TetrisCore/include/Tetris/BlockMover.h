f2e060634cc2351eb30b35f08b8b4a6e207c091b
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-10-17 00:42:12 +0000

    Use compilation firewall for BlockMover and Gravity classes.

--- trunk/TetrisCore/include/Tetris/BlockMover.h
+++ trunk/TetrisCore/include/Tetris/BlockMover.h
@@ -2,19 +2,15 @@
 #define TETRIS_BLOCKMOVER_H_INCLUDED
 
 
-#include "Tetris/Threading.h"
-
-
-namespace Poco
-{
-    class Timer;
-}
-
-
 namespace Tetris
 {
 
+    template<class Variable> class Protected;
     class Game;
+
+
+    class BlockMoverImpl;
+
 
     /**
      * BlockMover
@@ -24,36 +20,22 @@ namespace Tetris
      *
      * If no precalculated blocks are available then it will enter a waiting state.
      */
-    class BlockMover : boost::noncopyable
+    class BlockMover
     {
     public:
         BlockMover(const Protected<Game> & inGame, int inNumMovesPerSecond);
 
         ~BlockMover();
 
-        // Set the move speed.
         void setSpeed(int inNumMovesPerSecond);
 
         int speed() const;
 
-        enum MoveDownBehavior
-        {
-            MoveDownBehavior_Move,      // also move the block down at the same speed at left/right/rotate
-            MoveDownBehavior_DontMove,  // don't move the block down, let the gravity do the job (extra time allows for more AI precalculation)
-            MoveDownBehavior_Drop       // drop the block
-        };
-
-        // Define if and how the BlockMover should move the current block down.
-        void setMoveDownBehavior(MoveDownBehavior inMoveDown);
-
     private:
-        void onTimer(Poco::Timer & ioTimer);
-        void move();
+        BlockMover(const BlockMover &);
+        BlockMover & operator=(const BlockMover&);
 
-        Protected<Game> mGame;
-        boost::scoped_ptr<Poco::Timer> mTimer;
-        int mNumMovesPerSecond;
-        MoveDownBehavior mMoveDownBehavior;
+        BlockMoverImpl * mImpl;
     };
 
 } // namespace Tetris
--- trunk/TetrisCore/src/Gravity.cpp
+++ trunk/TetrisCore/src/Gravity.cpp
@@ -1,9 +1,11 @@
 #include "Tetris/Gravity.h"
-#include "Tetris/Game.h"
 #include "Tetris/GameStateNode.h"
 #include "Tetris/GameState.h"
+#include "Tetris/Game.h"
 #include "Tetris/Direction.h"
 #include "Tetris/Logging.h"
+#include "Tetris/Threading.h"
+#include "Poco/Timer.h"
 
 
 namespace Tetris
@@ -18,24 +20,56 @@ namespace Tetris
         100, 84, 84, 67, 67, 50
     };
 
+
     static const int cMaxLevel = sizeof(sIntervals)/sizeof(int) - 1;
 
-    Gravity::Gravity(const Protected<Game> & inThreadSafeGame) :
+
+    class GravityImpl
+    {
+    public:
+        GravityImpl(const Protected<Game> & inGame);
+
+        ~GravityImpl();
+
+        int getLevel() const;
+
+        // Number of rows per second
+        float currentSpeed() const;
+
+        // The higher the level the faster the blocks are falling.
+        // Levels go from 0 to 20. If the level is set to a
+        // bigger value then it is set to 20 instead.
+        void setLevel(int inLevel);
+
+    private:
+        GravityImpl(const GravityImpl &);
+        GravityImpl & operator=(const GravityImpl &);
+
+        void onTimerEvent(Poco::Timer & inTimer);
+
+        Protected<Game> mThreadSafeGame;
+        int mLevel;
+        mutable boost::mutex mLevelMutex;
+        Poco::Timer mTimer;
+    };
+
+
+    GravityImpl::GravityImpl(const Protected<Game> & inThreadSafeGame) :
         mThreadSafeGame(inThreadSafeGame),
         mLevel(0)
     {
-        mTimer.start(Poco::TimerCallback<Gravity>(*this, &Gravity::onTimerEvent));
+        mTimer.start(Poco::TimerCallback<GravityImpl>(*this, &GravityImpl::onTimerEvent));
         mTimer.setPeriodicInterval(sIntervals[getLevel()]);
     }
 
 
-    Gravity::~Gravity()
+    GravityImpl::~GravityImpl()
     {
         mTimer.stop();
     }
 
 
-    void Gravity::onTimerEvent(Poco::Timer & inTimer)
+    void GravityImpl::onTimerEvent(Poco::Timer & inTimer)
     {
         try
         {
@@ -63,25 +97,55 @@ namespace Tetris
     }
 
 
-    float Gravity::currentSpeed() const
+    float GravityImpl::currentSpeed() const
     {
         return static_cast<float>(1000.0 / static_cast<float>(sIntervals[getLevel()]));
     }
 
 
-    int Gravity::getLevel() const
+    int GravityImpl::getLevel() const
     {
         boost::mutex::scoped_lock lock(mLevelMutex);
         return mLevel;
     }
 
 
-    void Gravity::setLevel(int inLevel)
+    void GravityImpl::setLevel(int inLevel)
     {
         boost::mutex::scoped_lock lock(mLevelMutex);
         mLevel = std::min<int>(inLevel, cMaxLevel);
         mTimer.setPeriodicInterval(sIntervals[mLevel]);
     }
 
+    
+    Gravity::Gravity(const Protected<Game> & inGame) :
+        mImpl(new GravityImpl(inGame))
+    {
+    }
+
+    
+    Gravity::~Gravity()
+    {
+        delete mImpl;
+        mImpl = 0;
+    }
+
+    
+    int Gravity::getLevel() const
+    {
+        return mImpl->getLevel();
+    }
+
+
+    void Gravity::setLevel(int inLevel)
+    {
+        mImpl->setLevel(inLevel);
+    }
+
+
+    float Gravity::currentSpeed() const
+    {
+        return mImpl->currentSpeed();
+    }
 
 } // namespace Tetris
