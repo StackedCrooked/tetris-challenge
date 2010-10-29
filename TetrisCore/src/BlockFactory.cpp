c29aab9f4a6afb10a092160fac6482dee727194d
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-10-29 22:48:11 +0000

    Cleanup Block and BlockFactory Impls.

--- TetrisCore/src/BlockFactory.cpp
+++ TetrisCore/src/BlockFactory.cpp
@@ -12,32 +12,50 @@ namespace Tetris
     class BlockFactoryImpl
     {
     public:
-        BlockFactoryImpl(int inBagSize) :
-            mBagSize(inBagSize),
-            mCurrentIndex(0)
-        {
-        }
+        BlockFactoryImpl(int inBagSize);
 
+        BlockType getNext() const;
+
+    private:
         size_t mBagSize;
         mutable size_t mCurrentIndex;
         mutable BlockTypes mBag;
     };
 
 
-    BlockFactory::BlockFactory(int inBagSize) :
-        mImpl(new BlockFactoryImpl(inBagSize))
+    BlockFactoryImpl::BlockFactoryImpl(int inBagSize) :
+        mBagSize(inBagSize),
+        mCurrentIndex(0)
     {
         // Pick a new seed.
         srand(static_cast<unsigned int>(time(NULL)));
 
-        size_t totalSize = mImpl->mBagSize * cBlockTypeCount;
-        mImpl->mBag.reserve(totalSize);
+        size_t totalSize = mBagSize * cBlockTypeCount;
+        mBag.reserve(totalSize);
         for (size_t idx = 0; idx != totalSize; ++idx)
         {
             BlockType blockType = static_cast<BlockType>(1 + (idx % cBlockTypeCount));
-            mImpl->mBag.push_back(blockType);
+            mBag.push_back(blockType);
         }
-        std::random_shuffle(mImpl->mBag.begin(), mImpl->mBag.end());
+        std::random_shuffle(mBag.begin(), mBag.end());
+    }
+
+
+    BlockType BlockFactoryImpl::getNext() const
+    {
+        if (mCurrentIndex >= mBag.size())
+        {
+            // Reshuffle the bag.
+            std::random_shuffle(mBag.begin(), mBag.end());
+            mCurrentIndex = 0;
+        }
+        return mBag[mCurrentIndex++];
+    }
+
+
+    BlockFactory::BlockFactory(int inBagSize) :
+        mImpl(new BlockFactoryImpl(inBagSize))
+    {
     }
 
 
@@ -50,13 +68,7 @@ namespace Tetris
 
     BlockType BlockFactory::getNext() const
     {
-        if (mImpl->mCurrentIndex >= mImpl->mBag.size())
-        {
-            // Reshuffle the bag.
-            std::random_shuffle(mImpl->mBag.begin(), mImpl->mBag.end());
-            mImpl->mCurrentIndex = 0;
-        }
-        return mImpl->mBag[mImpl->mCurrentIndex++];
+        return mImpl->getNext();
     }
 
 } // namespace Tetris
