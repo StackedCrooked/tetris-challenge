a6185acbc43d90013ccc68d909dbd27c813f47c8
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-11-08 21:31:24 +0000

    Use ScopedReader where possible.

--- trunk/TetrisCore/src/Logger.cpp
+++ trunk/TetrisCore/src/Logger.cpp
@@ -57,7 +57,7 @@ namespace Tetris
         // Since the logging can be a slow operation we don't keep the Game object locked here.
         // We just copy the items, and log them afterwards.
         {
-            ScopedReaderAndWriter<Queue> queue(mProtectedQueue);        
+            ScopedReader<Queue> queue(mProtectedQueue);        
             for (size_t idx = 0; idx != queue->size(); ++idx)
             {
                 items.push_back((*queue.get())[idx]);
@@ -87,7 +87,7 @@ namespace Tetris
     {
         // Critical section: add the log message to the buffer
         {
-            ScopedReaderAndWriter<Queue> queue(mProtectedQueue);
+            ScopedReader<Queue> queue(mProtectedQueue);
             queue->push_back(GetMessage(inLogLevel, inMessage));
         }
     }
