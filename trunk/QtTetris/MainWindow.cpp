a1b7c442d7e2f7a1da63d9cc28981645479c0b28
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-12-17 23:58:34 +0000

    Add TetrisWidget

--- trunk/QtTetris/MainWindow.cpp
+++ trunk/QtTetris/MainWindow.cpp
@@ -1,14 +1,42 @@
 #include "MainWindow.h"
-#include "Tetris/BlockFactory.h"
+#include <QLayout>
+#include <QLabel>
+#include "Tetris/AutoPtrSupport.h"
+#include "Tetris/Block.h"
+#include "Tetris/Game.h"
+#include "Tetris/Threading.h"
+#include "Tetris/Grid.h"
+#include <iostream>
 
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
+using namespace Tetris;
+
+
+static Protected<Game> Tetris_CreateGame()
 {
+    return Protected<Game>(Create<Game>(TetrisWidget_NumRows(),
+                                        TetrisWidget_NumColumns()));
+}
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    mTetrisWidget(0)
+{
+    QWidget * centralWidget = new QWidget(this);
+
+    mTetrisWidget = new TetrisWidget(centralWidget, Tetris_CreateGame());
+
+    QHBoxLayout * hbox = new QHBoxLayout();
+    hbox->addWidget(new QLabel("before"), 1, Qt::AlignCenter);
+    hbox->addWidget(mTetrisWidget, 2, Qt::AlignCenter);
+    hbox->addWidget(new QLabel("after"), 1, Qt::AlignCenter);
+
+    centralWidget->setLayout(hbox);
+    setCentralWidget(centralWidget);
 }
 
 
 MainWindow::~MainWindow()
 {
-
 }
--- trunk/QtTetris/main.cpp
+++ trunk/QtTetris/main.cpp
@@ -2,6 +2,29 @@
 #include "MainWindow.h"
 
 
+int TetrisWidget_NumRows()
+{
+    return 20;
+}
+
+
+int TetrisWidget_NumColumns()
+{
+    return 10;
+}
+
+
+int Tetris_GetUnitWidth()
+{
+    return 20;
+}
+
+
+int Tetris_GetUnitHeight()
+{
+    return 20;
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
