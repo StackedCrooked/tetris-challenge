4e0131c4ac7395724dc3213188d9f427c6bacb08
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-12-25 21:39:39 +0000

    Add EvilBlockFactory untested.

---
+++ Tetris/include/Tetris/EvilBlockFactory.h
@@ -0,0 +1,37 @@
+#ifndef TETRIS_EVILBLOCKFACTORY_H
+#define TETRIS_EVILBLOCKFACTORY_H
+
+
+#include "Tetris/BlockFactory.h"
+#include "Tetris/Game.h"
+#include "Tetris/GameState.h"
+#include "Tetris/GameStateNode.h"
+#include "Tetris/NodeCalculator.h"
+#include "Tetris/Threading.h"
+#include "Tetris/WorkerPool.h"
+#include <boost/scoped_ptr.hpp>
+
+
+namespace Tetris {
+
+
+class EvilBlockFactory : public AbstractBlockFactory
+{
+public:
+    EvilBlockFactory(Protected<Game> inGame);
+
+    virtual BlockType getNext() const;
+
+private:
+    std::auto_ptr<NodeCalculator> createNodeCalculator(const BlockTypes & inBlockTypes);
+
+    Protected<Game> mGame;
+    mutable WorkerPool mWorkerPool;
+};
+
+
+} // namespace Tetris
+
+
+#endif // TETRIS_EVILBLOCKFACTORY_H
+
