245af77bd0f17e075b75910947cb26b3bc7fa6ec
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-12-25 20:33:53 +0000

    Introduce AbstractBlockFactory.

--- Tetris/include/Tetris/BlockFactory.h
+++ Tetris/include/Tetris/BlockFactory.h
@@ -5,31 +5,49 @@
 #include <memory>
 
 
-namespace Tetris
+namespace Tetris {
+
+
+class Block;
+class BlockFactoryImpl;
+typedef char BlockType;
+
+
+class AbstractBlockFactory
 {
- 
-    class Block;    
-    class BlockFactoryImpl;
-    typedef char BlockType;
-    
+public:
+    AbstractBlockFactory();
 
-    class BlockFactory
-    {
-    public:
-        // The size of the bag of blocks that shuffled and taken from.
-        BlockFactory(int inBagSize = 1);
+    virtual ~AbstractBlockFactory() = 0;
 
-        ~BlockFactory();
+    // Returns a random block type.
+    virtual BlockType getNext() const = 0;
 
-        // Returns a random block type.
-        BlockType getNext() const;
+private:
+    AbstractBlockFactory(const AbstractBlockFactory &);
+    AbstractBlockFactory& operator=(const AbstractBlockFactory&);
+};
 
-    private:
-        BlockFactory(const BlockFactory &);
-        BlockFactory& operator=(const BlockFactory&);
 
-        BlockFactoryImpl * mImpl;
-    };
+// The default block factory
+class BlockFactory : public AbstractBlockFactory
+{
+public:
+    // The size of the bag of blocks that shuffled and taken from.
+    BlockFactory(int inBagSize = 1);
+
+    virtual ~BlockFactory();
+
+    // Returns a random block type.
+    virtual BlockType getNext() const;
+
+private:
+    BlockFactory(const BlockFactory &);
+    BlockFactory& operator=(const BlockFactory&);
+
+    BlockFactoryImpl * mImpl;
+};
+
 
 } // namespace Tetris
 
