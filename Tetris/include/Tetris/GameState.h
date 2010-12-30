6cc9d905be82d73fcf322d8000e9fc483b638cc6
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2010-12-30 23:18:38 +0000

    Prepare refactoring for GameState class by undoing the pimpl.

--- Tetris/include/Tetris/GameState.h
+++ Tetris/include/Tetris/GameState.h
@@ -2,6 +2,7 @@
 #define TETRIS_GAMESTATE_H_INCLUDED
 
 
+#include "Tetris/Block.h"
 #include "Tetris/GameOver.h"
 #include "Tetris/Grid.h"
 #include <memory>
@@ -12,57 +13,11 @@ namespace Tetris
 {
 
     // Forward declarations    
-    class Block;
     class Evaluator;
 
 
     // Impl declaration
     class GameStateImpl;
-    class Stats;
-
-    
-    class GameState
-    {
-    public:
-        // Creates a new GameState
-        GameState(size_t inNumRows, size_t inNumColumns);
-        
-        GameState(const GameState &);
-
-        ~GameState();
-
-        const Grid & grid() const;
-
-        Grid & grid();
-
-        // The Block that was used in the commit(...) call.
-        const Block & originalBlock() const;
-
-        bool isGameOver() const;
-
-        // Calculates the quality of the playing field.
-        // Caches the value.
-        int quality(const Evaluator & inEvaluator) const;
-
-        // Checks if a activeBlock can be placed at a given location without
-        // overlapping with previously placed blocks.
-        bool checkPositionValid(const Block & inBlock, size_t inRowIdx, size_t inColIdx) const;
-
-        // Creates a copy of the current gamestate with the given active block committed.
-        // Use inGameOver = true to mark the new gamestate as "game over".
-        std::auto_ptr<GameState> commit(const Block & inBlock, GameOver inGameOver) const;
-
-        const Stats & stats() const;
-
-        void forceUpdateStats();
-
-    private:
-        // not allowed
-        GameState& operator=(const GameState&);
-
-        friend class GameStateImpl;
-        GameStateImpl * mImpl;
-    };
 
 
     // GameState stats
@@ -97,6 +52,103 @@ namespace Tetris
         int mNumTriples;
         int mNumTetrises;
         int mFirstOccupiedRow;
+    };
+
+
+    class Quality
+	{
+	public:
+		Quality() :
+			mIsInitialized(false),
+			mScore(0),
+			mNumHoles(0)
+		{
+		}
+
+		bool isInitialized() const
+		{
+			return mIsInitialized;
+		}
+
+		void setInitialized(bool inIsInitialized)
+		{
+			mIsInitialized = inIsInitialized;
+		}
+
+		int score() const
+		{
+			return mScore;
+		}
+
+		void setScore(int inScore)
+		{
+			mScore = inScore;
+		}
+
+		int numHoles() const
+		{
+			return mNumHoles;
+		}
+
+		void setNumHoles(int inNumHoles)
+		{
+			mNumHoles = inNumHoles;
+		}
+
+		void reset()
+		{
+			mScore = 0;
+			mNumHoles = 0;
+		}
+
+	private:
+		bool mIsInitialized;
+		int mScore;
+		int mNumHoles;
+	};
+
+
+    class GameState
+    {
+    public:
+        // Creates a new GameState
+        GameState(size_t inNumRows, size_t inNumColumns);
+
+        const Grid & grid() const;
+
+        Grid & grid();
+
+        // The Block that was used in the commit(...) call.
+        const Block & originalBlock() const;
+
+        bool isGameOver() const;
+
+        // Calculates the quality of the playing field.
+        // Caches the value.
+        int quality(const Evaluator & inEvaluator) const;
+
+        // Checks if a activeBlock can be placed at a given location without
+        // overlapping with previously placed blocks.
+        bool checkPositionValid(const Block & inBlock, size_t inRowIdx, size_t inColIdx) const;
+
+        // Creates a copy of the current gamestate with the given active block committed.
+        // Use inGameOver = true to mark the new gamestate as "game over".
+        std::auto_ptr<GameState> commit(const Block & inBlock, GameOver inGameOver) const;
+
+        const Stats & stats() const;
+
+        void forceUpdateStats();
+
+    private:
+        void solidifyBlock(const Block & inBlock);
+        
+        void clearLines();
+
+        Grid mGrid;
+        Block mOriginalBlock;
+        bool mIsGameOver;
+        Stats mStats;
+        mutable Quality mQuality;
     };
 
 
--- Tetris/src/GameState.cpp
+++ Tetris/src/GameState.cpp
@@ -9,397 +9,298 @@
 #include <vector>
 
 
-namespace Tetris
+namespace Tetris {
+
+
+Stats::Stats() :
+    mNumLines(0),
+    mNumSingles(0),
+    mNumDoubles(0),
+    mNumTriples(0),
+    mNumTetrises(0),
+    mFirstOccupiedRow(0)
 {
-    
-    class Quality
+}
+
+
+int Stats::score() const
+{
+    return 40 * mNumSingles + 100 * mNumDoubles + 300 * mNumTriples + 1200 * mNumTetrises;
+}
+
+
+int Stats::numLines() const
+{
+    return mNumLines;
+}
+
+
+int Stats::numSingles() const
+{
+    return mNumSingles;
+}
+
+
+int Stats::numDoubles() const
+{
+    return mNumDoubles;
+}
+
+
+int Stats::numTriples() const
+{
+    return mNumTriples;
+}
+
+
+int Stats::numTetrises() const
+{
+    return mNumTetrises;
+}
+
+
+int Stats::firstOccupiedRow() const
+{
+    return mFirstOccupiedRow;
+}
+
+
+// Use zero based index!
+int Stats::numLines(size_t idx) const
+{
+    switch (idx)
     {
-    public:
-        Quality() :
-            mIsInitialized(false),
-            mScore(0),
-            mNumHoles(0)
-        {
-        }
-
-        bool isInitialized() const
-        {
-            return mIsInitialized;
-        }
-
-        void setInitialized(bool inIsInitialized)
-        {
-            mIsInitialized = inIsInitialized;
-        }
-
-        int score() const
-        {
-            return mScore;
-        }
-
-        void setScore(int inScore)
-        {
-            mScore = inScore;
-        }
-
-        int numHoles() const
-        {
-            return mNumHoles;
-        }
-
-        void setNumHoles(int inNumHoles)
-        {
-            mNumHoles = inNumHoles;
-        }
-
-        void reset()
-        {
-            mScore = 0;
-            mNumHoles = 0;
-        }
-
-    private:
-        bool mIsInitialized;
-        int mScore;
-        int mNumHoles;
-    };
+        case 0: return mNumSingles;
+        case 1: return mNumDoubles;
+        case 2: return mNumTriples;
+        case 3: return mNumTetrises;
+        default: throw std::invalid_argument("Invalid number of lines scored requested.");
+    }
+    return 0; // compiler happy
+}
 
 
-    Stats::Stats() :
-        mNumLines(0),
-        mNumSingles(0),
-        mNumDoubles(0),
-        mNumTriples(0),
-        mNumTetrises(0),
-        mFirstOccupiedRow(0)
+GameState::GameState(size_t inNumRows, size_t inNumColumns) :
+    mGrid(inNumRows, inNumColumns, BlockType_Nil),
+    mOriginalBlock(BlockType_L, Rotation(0), Row(0), Column(0)),
+    mIsGameOver(false),
+    mStats(),
+    mQuality()
+{
+    mStats.mFirstOccupiedRow = inNumRows;
+}
+
+
+bool GameState::checkPositionValid(const Block & inBlock, size_t inRowIdx, size_t inColIdx) const
+{
+    const Grid & blockGrid(inBlock.grid());
+    if (inColIdx < blockGrid.columnCount() &&
+        (inRowIdx + blockGrid.rowCount()) < static_cast<size_t>(mStats.firstOccupiedRow()))
     {
+        return true;
     }
 
-    int Stats::score() const
+    if (inRowIdx >= mGrid.rowCount())
     {
-        return 40 * mNumSingles + 100 * mNumDoubles + 300 * mNumTriples + 1200 * mNumTetrises;
+        return false;
     }
 
-    int Stats::numLines() const
+    for (size_t r = 0; r != blockGrid.rowCount(); ++r)
     {
-        return mNumLines;
-    }
-
-    int Stats::numSingles() const
-    {
-        return mNumSingles;
-    }
-
-    int Stats::numDoubles() const
-    {
-        return mNumDoubles;
-    }
-
-    int Stats::numTriples() const
-    {
-        return mNumTriples;
-    }
-
-    int Stats::numTetrises() const
-    {
-        return mNumTetrises;
-    }
-
-    int Stats::firstOccupiedRow() const
-    {
-        return mFirstOccupiedRow;
-    }
-
-    // Use zero based index!
-    int Stats::numLines(size_t idx) const
-    {
-        switch (idx)
-        {
-            case 0: return mNumSingles;
-            case 1: return mNumDoubles;
-            case 2: return mNumTriples;
-            case 3: return mNumTetrises;
-            default: throw std::invalid_argument("Invalid number of lines scored requested.");
-        }
-        return 0; // compiler happy
-    }
-
-
-    class GameStateImpl
-    {
-    public:
-        GameStateImpl(size_t inNumRows, size_t inNumColumns);
-
-        void solidifyBlock(const Block & inBlock);
-        
-        void clearLines();
-
-        void forceUpdateStats();
-
-        bool checkPositionValid(const Block & inBlock, size_t inRowIdx, size_t inColIdx) const;
-
-    private:
-        friend class GameState;
-        Grid mGrid;
-        Stats mStats;
-        bool mIsGameOver;
-        Block mOriginalBlock;
-        mutable Quality mQuality;
-    };
-
-
-    GameStateImpl::GameStateImpl(size_t inNumRows, size_t inNumColumns) :
-        mGrid(inNumRows, inNumColumns, BlockType_Nil),
-        mStats(),
-        mIsGameOver(false),
-        mOriginalBlock(BlockType_L, Rotation(0), Row(0), Column(0)),
-        mQuality()
-    {
-        mStats.mFirstOccupiedRow = inNumRows;
-    }
-
-
-    bool GameStateImpl::checkPositionValid(const Block & inBlock, size_t inRowIdx, size_t inColIdx) const
-    {
-        const Grid & blockGrid(inBlock.grid());
-        if (inColIdx < blockGrid.columnCount() &&
-            (inRowIdx + blockGrid.rowCount()) < static_cast<size_t>(mStats.firstOccupiedRow()))
-        {
-            return true;
-        }
-
-        if (inRowIdx >= mGrid.rowCount())
+        size_t rowIdx = inRowIdx + r;
+        if (rowIdx >= mGrid.rowCount())
         {
             return false;
         }
 
-        for (size_t r = 0; r != blockGrid.rowCount(); ++r)
+        for (size_t c = 0; c != blockGrid.columnCount(); ++c)
         {
-            size_t rowIdx = inRowIdx + r;
-            if (rowIdx >= mGrid.rowCount())
+            size_t colIdx = inColIdx + c;
+            if (colIdx >= mGrid.columnCount())
             {
                 return false;
             }
 
-            for (size_t c = 0; c != blockGrid.columnCount(); ++c)
+            if (blockGrid.get(r, c) && mGrid.get(rowIdx, colIdx))
             {
-                size_t colIdx = inColIdx + c;
-                if (colIdx >= mGrid.columnCount())
-                {
-                    return false;
-                }
-
-                if (blockGrid.get(r, c) && mGrid.get(rowIdx, colIdx))
-                {
-                    return false;
-                }
+                return false;
             }
         }
-        return true;
     }
+    return true;
+}
 
 
-    void GameStateImpl::forceUpdateStats()
+void GameState::forceUpdateStats()
+{
+    for (size_t r = 0; r != mGrid.rowCount(); ++r)
     {
-        for (size_t r = 0; r != mGrid.rowCount(); ++r)
+        for (size_t c = 0; c != mGrid.columnCount(); ++c)
         {
-            for (size_t c = 0; c != mGrid.columnCount(); ++c)
+            if (mGrid.get(r, c) != BlockType_Nil)
             {
-                if (mGrid.get(r, c) != BlockType_Nil)
-                {
-                    mStats.mFirstOccupiedRow = r;
-                    return;
-                }
+                mStats.mFirstOccupiedRow = r;
+                return;
             }
         }
     }
+}
 
 
-    void GameStateImpl::solidifyBlock(const Block & inBlock)
+void GameState::solidifyBlock(const Block & inBlock)
+{
+    const Grid & grid = inBlock.grid();
+    for (size_t r = 0; r != grid.rowCount(); ++r)
     {
-        const Grid & grid = inBlock.grid();
-        for (size_t r = 0; r != grid.rowCount(); ++r)
+        for (size_t c = 0; c != grid.columnCount(); ++c)
         {
-            for (size_t c = 0; c != grid.columnCount(); ++c)
+            if (grid.get(r, c) != BlockType_Nil)
             {
-                if (grid.get(r, c) != BlockType_Nil)
+                int gridRow = inBlock.row() + r;
+                int gridCol = inBlock.column() + c;
+                mGrid.set(gridRow, gridCol, inBlock.type());
+                if (gridRow < mStats.mFirstOccupiedRow)
                 {
-                    int gridRow = inBlock.row() + r;
-                    int gridCol = inBlock.column() + c;
-                    mGrid.set(gridRow, gridCol, inBlock.type());
-                    if (gridRow < mStats.mFirstOccupiedRow)
-                    {
-                        mStats.mFirstOccupiedRow = gridRow;
-                    }
+                    mStats.mFirstOccupiedRow = gridRow;
                 }
             }
         }
     }
+}
 
     
-    void GameStateImpl::clearLines()
+void GameState::clearLines()
+{
+    int numLines = 0;
+    unsigned int r = mOriginalBlock.row() + mOriginalBlock.rowCount() - 1;
+    for (; r >= mStats.firstOccupiedRow(); --r)
     {
-        int numLines = 0;
-        int r = mOriginalBlock.row() + mOriginalBlock.rowCount() - 1;
-        for (; r >= mStats.firstOccupiedRow(); --r)
+        unsigned int c = 0;
+        bool line = true;
+        for (; c < mGrid.columnCount(); ++c)
         {
-            int c = 0;
-            bool line = true;
-            for (; c < mGrid.columnCount(); ++c)
+            BlockType value = mGrid.get(r, c);
+            if (numLines > 0)
             {
-                BlockType value = mGrid.get(r, c);
-                if (numLines > 0)
-                {
-                    mGrid.set(r + numLines, c, value);
-                }
+                mGrid.set(r + numLines, c, value);
+            }
 
-                if (!value)
+            if (!value)
+            {
+                line = false;
+                if (numLines == 0)
                 {
-                    line = false;
-                    if (numLines == 0)
-                    {
-                        break;
-                    }
+                    break;
                 }
             }
-            if (line)
-            {
-                numLines++;
-            }
         }
-
-        if (numLines > 0)
+        if (line)
         {
-            BlockType * gridBegin = const_cast<BlockType*>(&(mGrid.get(mStats.firstOccupiedRow(), 0)));
-            memset(&gridBegin[0], 0, numLines * mGrid.columnCount());
+            numLines++;
         }
+    }
+
+    if (numLines > 0)
+    {
+        BlockType * gridBegin = const_cast<BlockType*>(&(mGrid.get(mStats.firstOccupiedRow(), 0)));
+        memset(&gridBegin[0], 0, numLines * mGrid.columnCount());
+    }
 		
-        Assert(mStats.mFirstOccupiedRow + numLines <= mGrid.rowCount());
-        mStats.mFirstOccupiedRow += numLines;
-        mStats.mNumLines += numLines;
+    Assert(mStats.mFirstOccupiedRow + numLines <= static_cast<int>(mGrid.rowCount()));
+    mStats.mFirstOccupiedRow += numLines;
+    mStats.mNumLines += numLines;
 
-        switch (numLines)
+    switch (numLines)
+    {
+        case 0:
         {
-            case 0:
-            {
-                // Do nothing.
-                break;
-            }
-            case 1:
-            {
-                mStats.mNumSingles++;
-                break;
-            }
-            case 2:
-            {
-                mStats.mNumDoubles++;
-                break;
-            }
-            case 3:
-            {
-                mStats.mNumTriples++;
-                break;
-            }
-            case 4:
-            {
-                mStats.mNumTetrises++;
-                break;
-            }
-            default:
-            {
-                throw std::logic_error("Invalid number of lines scored! ...?");
-            }
+            // Do nothing.
+            break;
+        }
+        case 1:
+        {
+            mStats.mNumSingles++;
+            break;
+        }
+        case 2:
+        {
+            mStats.mNumDoubles++;
+            break;
+        }
+        case 3:
+        {
+            mStats.mNumTriples++;
+            break;
+        }
+        case 4:
+        {
+            mStats.mNumTetrises++;
+            break;
+        }
+        default:
+        {
+            throw std::logic_error("Invalid number of lines scored! ...?");
         }
     }
+}
 
 
-    GameState::GameState(size_t inNumRows, size_t inNumColumns) :
-        mImpl(new GameStateImpl(inNumRows, inNumColumns))
+bool GameState::isGameOver() const
+{
+    return mIsGameOver;
+}
+
+
+const Grid & GameState::grid() const
+{
+    return mGrid;
+}
+
+
+Grid & GameState::grid()
+{
+    return mGrid;
+}
+
+
+int GameState::quality(const Evaluator & inEvaluator) const
+{
+    if (!mQuality.isInitialized())
     {
+        mQuality.reset();
+        mQuality.setScore(inEvaluator.evaluate(*this));
+        mQuality.setInitialized(true);
     }
+    return mQuality.score();
+}
 
 
-    GameState::GameState(const GameState & rhs) :
-        mImpl(new GameStateImpl(*rhs.mImpl))
+const Block & GameState::originalBlock() const
+{
+    return mOriginalBlock;
+}
+
+
+std::auto_ptr<GameState> GameState::commit(const Block & inBlock, GameOver inGameOver) const
+{
+    std::auto_ptr<GameState> result(new GameState(*this));
+    result->mIsGameOver = inGameOver.get();
+    result->mQuality.setInitialized(false);
+    if (!inGameOver.get())
     {
-        
+        result->solidifyBlock(inBlock);
     }
+    result->mOriginalBlock = inBlock;
+    result->clearLines();
+    return result;
+}
 
 
-    GameState::~GameState()
-    {
-        delete mImpl;
-        mImpl = 0;
-    }
+const Stats & GameState::stats() const
+{
+    return mStats;
+}
 
-
-    bool GameState::isGameOver() const
-    {
-        return mImpl->mIsGameOver;
-    }
-
-
-    const Grid & GameState::grid() const
-    {
-        return mImpl->mGrid;
-    }
-
-
-    Grid & GameState::grid()
-    {
-        return mImpl->mGrid;
-    }
-
-
-    int GameState::quality(const Evaluator & inEvaluator) const
-    {
-        if (!mImpl->mQuality.isInitialized())
-        {
-            mImpl->mQuality.reset();
-            mImpl->mQuality.setScore(inEvaluator.evaluate(*this));
-            mImpl->mQuality.setInitialized(true);
-        }
-        return mImpl->mQuality.score();
-    }
-
-
-    bool GameState::checkPositionValid(const Block & inBlock, size_t inRowIdx, size_t inColIdx) const
-    {
-        return mImpl->checkPositionValid(inBlock, inRowIdx, inColIdx);
-    }
-
-
-    const Block & GameState::originalBlock() const
-    {
-        return mImpl->mOriginalBlock;
-    }
-
-
-    std::auto_ptr<GameState> GameState::commit(const Block & inBlock, GameOver inGameOver) const
-    {
-        std::auto_ptr<GameState> result(new GameState(*this));
-        result->mImpl->mIsGameOver = inGameOver.get();
-        result->mImpl->mQuality.setInitialized(false);
-        if (!inGameOver.get())
-        {
-            result->mImpl->solidifyBlock(inBlock);
-        }
-        result->mImpl->mOriginalBlock = inBlock;
-        result->mImpl->clearLines();
-        return result;
-    }
-
-
-    const Stats & GameState::stats() const
-    {
-        return mImpl->mStats;
-    }
-
-
-    void GameState::forceUpdateStats()
-    {
-        mImpl->forceUpdateStats();
-    }
 
 } // namespace Tetris
 
