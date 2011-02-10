becdc7a83e9476413ef31710187a4108e6903af5
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-02-10 19:15:53 +0000

    Add shared_ptr, just for testing.

--- Tetris/include/Tetris/Utilities.h
+++ Tetris/include/Tetris/Utilities.h
@@ -5,16 +5,89 @@
 namespace Tetris {
 
 
-/**
- * DivideByTwo
- *
- * Rounded division by 2 for integral values.
- */
 template<class T>
-T DivideByTwo(T inValue)
+static T DivideByTwo(T inValue)
 {
-    return (int)(0.5 + ((float)inValue / (float)2.0));
+    return static_cast<int>(0.5 + 0.5 * inValue);
 }
+
+
+template<class T>
+class shared_ptr
+{
+public:
+    struct Impl
+    {
+        Impl(T * inValue) :
+            mRefCount(1),
+            mValue(inValue)
+        {
+        }
+        int mRefCount;
+        T * mValue;
+    };
+
+    shared_ptr(T * inValue = 0) :
+        mImpl(new Impl(inValue))
+    {
+    }
+
+    shared_ptr(const shared_ptr & rhs) :
+        mImpl(rhs.mImpl)
+    {
+        mImpl->mRefCount++;
+    }
+
+    ~shared_ptr()
+    {
+        unref();
+    }
+
+    shared_ptr & operator=(const shared_ptr & rhs)
+    {
+        if (this != &rhs)
+        {
+            rhs.mImpl->mRefCount++;
+            unref();
+            mImpl = rhs.mImpl;
+        }
+        return *this;
+    }
+
+    void reset()
+    {
+        unref();
+    }
+
+    void reset(T * inValue)
+    {
+        unref();
+        mImpl = new Impl(inValue);
+    }
+
+    T & operator* () const
+    {
+        return *mImpl->mValue;
+    }
+
+    T * operator-> () const
+    {
+        return mImpl->mValue;
+    }
+
+private:
+    inline void unref()
+    {
+        if (--mImpl->mRefCount == 0)
+        {
+            delete mImpl->mValue;
+            delete mImpl;
+            mImpl = 0;
+        }
+    }
+
+    Impl * mImpl;
+};
 
 
 } // namespace Tetris
