715ffdad18192d4c45d1e4988fb4ba6e0d7d8535
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-02-19 19:17:04 +0000

    Some tweaking.

--- trunk/QtTetris/main.cpp
+++ trunk/QtTetris/main.cpp
@@ -29,6 +29,8 @@ int Tetris_GetSquareHeight()
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    a.setApplicationName("Piratris");
+    a.setApplicationVersion("0.0 alpha");
     Tetris::MainThread::Initializer scopedInit;
     MainWindow w;
     w.show();
