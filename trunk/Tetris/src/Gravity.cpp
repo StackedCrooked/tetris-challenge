8d2af3b6f780e1ed79a6730bddd93738363db0c5
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-02-22 19:47:51 +0000

    Refactory to use Gravity::Impl.

--- trunk/Tetris/src/Gravity.cpp
+++ trunk/Tetris/src/Gravity.cpp
@@ -6,152 +6,123 @@
 #include "Tetris/Game.h"
 #include "Tetris/Direction.h"
 #include "Tetris/Logging.h"
-#include "Tetris/Threading.h"
 #include "Tetris/MakeString.h"
 #include "Tetris/Assert.h"
 #include "Poco/Timer.h"
 #include "Poco/Stopwatch.h"
+#include <boost/noncopyable.hpp>
 #include <algorithm>
 
 
-namespace Tetris
+namespace Tetris {
+
+
+// Number of milliseconds between two drops.
+static const int sIntervals[] =
 {
+    887, 820, 753, 686, 619,
+    552, 469, 368, 285, 184,
+    167, 151, 134, 117, 100,
+    100, 84, 84, 67, 67, 50
+};
 
-    // Number of milliseconds between two drops.
-    static const int sIntervals[] =
+
+const int cIntervalCount = sizeof(sIntervals)/sizeof(int);
+extern const int cMaxLevel = sizeof(sIntervals)/sizeof(int) - 1;
+
+
+struct Gravity::Impl : boost::noncopyable
+{
+    Impl(ThreadSafe<Game> inThreadSafeGame) :
+        mThreadSafeGame(inThreadSafeGame)
     {
-        887, 820, 753, 686, 619,
-        552, 469, 368, 285, 184,
-        167, 151, 134, 117, 100,
-        100, 84, 84, 67, 67, 50
-    };
-
-    const int cIntervalCount = sizeof(sIntervals)/sizeof(int);
-    extern const int cMaxLevel = sizeof(sIntervals)/sizeof(int) - 1;
-
-
-    class GravityImpl
-    {
-    public:
-        GravityImpl(const ThreadSafe<Game> & inGame);
-
-        ~GravityImpl();
-
-        // Number of rows per second
-        double speed() const;
-
-        static double CalculateSpeed(int inLevel);
-
-        // Interval between two moves expressed in milliseconds.
-        int interval() const;
-
-    private:
-        GravityImpl(const GravityImpl &);
-        GravityImpl & operator=(const GravityImpl &);
-
-        void onTimerEvent(Poco::Timer & inTimer);
-
-        ThreadSafe<Game> mThreadSafeGame;
-        int mLevel;
-        Poco::Timer mTimer;
-        Poco::Stopwatch mStopwatch;
-    };
-
-
-    GravityImpl::GravityImpl(const ThreadSafe<Game> & inThreadSafeGame) :
-        mThreadSafeGame(inThreadSafeGame),
-        mLevel(0),
-        mTimer(),
-        mStopwatch()
-    {
-        ScopedReader<Game> rgame(mThreadSafeGame);
-        mTimer.start(Poco::TimerCallback<GravityImpl>(*this, &GravityImpl::onTimerEvent));
-        mTimer.setPeriodicInterval(sIntervals[rgame->level()]);
-        mStopwatch.start();
     }
 
-
-    GravityImpl::~GravityImpl()
+    ~Impl()
     {
-        mTimer.stop();
     }
 
+    void onTimerEvent(Poco::Timer & timer);
 
-    int GravityImpl::interval() const
+    int interval() const
     {
-        return static_cast<int>(0.5 + speed() / 1000.0);
+        return static_cast<int>(0.5 + CalculateSpeed(mLevel) / 1000.0);
     }
 
-
-    void GravityImpl::onTimerEvent(Poco::Timer & )
-    {
-        try
-        {
-            int oldLevel = mLevel;
-            if (mStopwatch.elapsed() > 1000  * interval())
-            {
-                mStopwatch.restart();
-                ScopedReaderAndWriter<Game> game(mThreadSafeGame);
-                if (game->isGameOver() || game->isPaused())
-                {
-                    return;
-                }
-                game->move(MoveDirection_Down);
-                mLevel = game->level();
-                if (mLevel > cMaxLevel)
-                {
-                    mLevel = cMaxLevel;
-                }
-            }
-            if (mLevel != oldLevel)
-            {
-                Assert(mLevel < cIntervalCount);
-                int newLevel = sIntervals[mLevel];
-                LogInfo(MakeString() << "Set level to " << mLevel << ".");
-                mTimer.setPeriodicInterval(newLevel);
-            }
-        }
-        catch (const std::exception & inException)
-        {
-            LogError(inException.what());
-        }
-    }
-
-
-    double GravityImpl::speed() const
-    {
-        return CalculateSpeed(mLevel);
-    }
-
-
-    double GravityImpl::CalculateSpeed(int inLevel)
+    static double CalculateSpeed(int inLevel)
     {
         return static_cast<double>(1000.0 / static_cast<double>(sIntervals[inLevel]));
     }
 
+    ThreadSafe<Game> mThreadSafeGame;
+    int mLevel;
+    Poco::Timer mTimer;
+    Poco::Stopwatch mStopwatch;
+};
 
-    Gravity::Gravity(const ThreadSafe<Game> & inGame) :
-        mImpl(new GravityImpl(inGame))
+
+Gravity::Gravity(const ThreadSafe<Game> & inThreadSafeGame) :
+    mImpl(new Impl(inThreadSafeGame))
+{
+    ScopedReader<Game> rgame(mImpl->mThreadSafeGame);
+    mImpl->mTimer.start(Poco::TimerCallback<Gravity::Impl>(*mImpl, &Gravity::Impl::onTimerEvent));
+    mImpl->mTimer.setPeriodicInterval(sIntervals[rgame->level()]);
+    mImpl->mStopwatch.start();
+}
+
+
+Gravity::~Gravity()
+{
+    delete mImpl;
+    mImpl = 0;
+}
+
+
+void Gravity::Impl::onTimerEvent(Poco::Timer & )
+{
+    try
     {
+        int oldLevel = mLevel;
+        if (mStopwatch.elapsed() > 1000  * interval())
+        {
+            mStopwatch.restart();
+            ScopedReaderAndWriter<Game> game(mThreadSafeGame);
+            if (game->isGameOver() || game->isPaused())
+            {
+                return;
+            }
+            game->move(MoveDirection_Down);
+            mLevel = game->level();
+            if (mLevel > cMaxLevel)
+            {
+                mLevel = cMaxLevel;
+            }
+        }
+        if (mLevel != oldLevel)
+        {
+            Assert(mLevel < cIntervalCount);
+            int newLevel = sIntervals[mLevel];
+            LogInfo(MakeString() << "Set level to " << mLevel << ".");
+            mTimer.setPeriodicInterval(newLevel);
+        }
     }
-
-
-    Gravity::~Gravity()
+    catch (const std::exception & inException)
     {
-        delete mImpl;
-        mImpl = 0;
+        LogError(inException.what());
     }
+}
 
 
-    double Gravity::speed() const
-    {
-        return mImpl->speed();
-    }
+double Gravity::speed() const
+{
+    return CalculateSpeed(mImpl->mLevel);
+}
 
 
-    double Gravity::CalculateSpeed(int inLevel)
-    {
-        return GravityImpl::CalculateSpeed(inLevel);
-    }
+double Gravity::CalculateSpeed(int inLevel)
+{
+    return Impl::CalculateSpeed(inLevel);
+}
+
 
 } // namespace Tetris
