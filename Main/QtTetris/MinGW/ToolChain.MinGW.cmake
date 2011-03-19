a8d8e44e7985faf3a9039d94e7189e002ceefad0
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-03-19 21:55:00 +0000

    Cleanup for the ToolChain experiment.

---
+++ Main/QtTetris/MinGW/ToolChain.MinGW.cmake
@@ -0,0 +1,5 @@
+SET(CMAKE_SYSTEM_NAME     Windows)
+SET(CMAKE_C_COMPILER      i586-mingw32msvc-gcc)
+SET(CMAKE_CXX_COMPILER    i586-mingw32msvc-g++)
+SET(CMAKE_FIND_ROOT_PATH  /usr/i586-mingw32msvc)
+
