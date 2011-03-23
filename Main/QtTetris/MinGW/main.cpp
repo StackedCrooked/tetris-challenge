a3a8af71655b54f343196c535ba1c90ee8c32a76
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-03-23 18:16:10 +0000

    MinGW makefile almost fine.

--- Main/QtTetris/MinGW/main.cpp
+++ Main/QtTetris/MinGW/main.cpp
@@ -1,8 +1,42 @@
-#include <stdio.h>
+#include <QtGui/QApplication>
+#include "MainWindow.h"
+#include "Futile/MainThread.h"
 
-int main()
+
+using Futile::MainThread;
+
+
+int Tetris_RowCount()
 {
-   printf("Hello world\n");
-   return 0;
+    return 20;
 }
 
+
+int Tetris_ColumnCount()
+{
+    return 10;
+}
+
+
+int Tetris_GetSquareWidth()
+{
+    return 20;
+}
+
+
+int Tetris_GetSquareHeight()
+{
+    return 20;
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    a.setApplicationName("QtTetris");
+    a.setApplicationVersion("0.0 alpha");
+    MainThread::Initializer scopedInit;
+    MainWindow w;
+    w.show();
+
+    return a.exec();
+}
