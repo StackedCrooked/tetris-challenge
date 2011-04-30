2e4560ca0a0b41a6d286ec9752e0a44ab0b35ddd
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-04-30 09:51:26 +0000

    Remove old code.

--- trunk/Main/Futile/src/Enum.cpp
+++ trunk/Main/Futile/src/Enum.cpp
@@ -4,11 +4,4 @@
 namespace Futile {
 
 
-//#define Futile_DefineEnumInfoSpecialization(Tag, Size, Enumerator, First, Last) \
-//    EnumInfo<Tag>::Values EnumInfo<Tag>::values = \
-//        { BOOST_PP_LIST_ENUM(BOOST_PP_TUPLE_TO_LIST(Size, Enumerator)) }; \
-//    EnumInfo<Tag>::Names EnumInfo<Tag>::names = \
-//        { BOOST_PP_LIST_ENUM(BOOST_PP_LIST_TRANSFORM(Futile_ToString, Size, BOOST_PP_TUPLE_TO_LIST(Size, Enumerator))) };
-
-
 } // namespace Futile
