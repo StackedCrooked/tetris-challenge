99858f896d71f69aeff48090080b48f3aafafdba
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-06-16 17:53:11 +0000

    Apply formatter.

--- trunk/Main/Scripts/Format/FormatCode.sh
+++ trunk/Main/Scripts/Format/FormatCode.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ASTYLE_ARGS="--indent=spaces=4 --brackets=break --indent-switches --indent-labels --pad-header --unpad-paren --convert-tabs --align-pointer=middle --suffix=none --lineend=linux --recursive"
+ASTYLE_ARGS="--indent=spaces=4 --brackets=break --indent-switches --indent-labels --pad-header --unpad-paren --convert-tabs --align-pointer=middle --suffix=none --lineend=linux"
 
 for File in $* ; do
     astyle $ASTYLE_ARGS $File
