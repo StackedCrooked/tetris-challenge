b8a91e2d1e1fe9efcbbdaf73f82163f9ddb07eb7
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-07-01 20:42:04 +0000

    Remove MemoryPool.

--- Tetris/testsuite/src/TetrisTestSuite.cpp
+++ Tetris/testsuite/src/TetrisTestSuite.cpp
@@ -1,5 +1,4 @@
 #include "TetrisTestSuite.h"
-#include "MemoryPoolTest.h"
 #include "WorkerTest.h"
 #include "WorkerPoolTest.h"
 #include "NodeCalculatorTest.h"
@@ -12,7 +11,6 @@ CppUnit::Test* TetrisCoreTestSuite::suite()
     CppUnit::TestSuite* pSuite = new CppUnit::TestSuite("TetrisCoreTestSuite");
 
     pSuite->addTest(NodeTest::suite());
-    pSuite->addTest(MemoryPoolTest::suite());
     pSuite->addTest(GenericGridTest::suite());
     pSuite->addTest(WorkerTest::suite());
     pSuite->addTest(WorkerPoolTest::suite());
