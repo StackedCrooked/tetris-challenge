543881749bbba11ddd1ab6497683943ddb3d6246
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-07-20 20:12:42 +0000

    Remove obsolete stuff.

--- 3rdParty/Boost_1_42_0/lib/CheckoutLibs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PLATFORM=`uname -o`
-ARCH=`arch`
-URL="https://tetris-challenge.googlecode.com/svn/attic/Boost_1_42_0/lib/$PLATFORM/$ARCH"
-mkdir -p $PLATFORM
-cd $PLATFORM
-echo "Checking out libs from $URL"
-svn co $URL
