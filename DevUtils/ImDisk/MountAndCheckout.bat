171666c1fc4f650e8499efb4174fec41b935a7b1
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-07-21 03:02:46 +0000

    Remove unused DevUtils.

--- DevUtils/ImDisk/MountAndCheckout.bat
+++
@@ -1,2 +0,0 @@
-imdisk -a -s 1200M -m M: -p "/fs:ntfs /q /c /y"
-svn co --username=francis.rammeloo https://tetris-challenge.googlecode.com/svn/trunk M:\tetris-challenge
--- DevUtils/Make/MakeXcode.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p Projects
-rm -rf Projects/Xcode
-mkdir Projects/Xcode
-cd Projects/Xcode
-cmake -G Xcode ../..
-cd ../..
-echo
-echo "Created Xcode projects:"
-find Projects/Xcode -name "*.xcodeproj" | grep -v CMakeTmp
