cb8d6c3bce1f11983b95471ff995c0cd7bea62b4
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-09-16 17:33:16 +0000

    Add Synchronized. Untested.

--- Futile/include/Futile/Atomic.h
+++ Futile/include/Futile/Atomic.h
@@ -3,6 +3,7 @@
 
 
 #include "Futile/Threading.h"
+#include <boost/function.hpp>
 
 
 namespace Futile {
@@ -28,6 +29,37 @@ public:
     {
         ScopedLock lock(mMutex);
         mValue = inValue;
+    }
+
+private:
+    mutable Mutex mMutex;
+    T mValue;
+};
+
+
+template<typename T>
+class Synchronized
+{
+public:
+    Synchronized(T inValue = T()) :
+        mValue(inValue)
+    {
+    }
+
+    typedef boost::function<void(const T &)> Reader;
+
+    void apply(Reader reader) const
+    {
+        ScopedLock lock(mMutex);
+        reader(mValue);
+    }
+
+    typedef boost::function<void(T &)> Writer;
+
+    void apply(Writer writer)
+    {
+        ScopedLock lock(mMutex);
+        writer(mValue);
     }
 
 private:
