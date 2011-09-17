06743fc2574264465c3dc093187a7d6bdb18df56
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-09-17 19:38:49 +0000

    Tetris/testsuite: Remove NodeTest tests.

--- Tetris/testsuite/src/TetrisTestSuite.cpp
+++ Tetris/testsuite/src/TetrisTestSuite.cpp
@@ -2,14 +2,12 @@
 #include "WorkerTest.h"
 #include "WorkerPoolTest.h"
 #include "NodeCalculatorTest.h"
-#include "NodeTest.h"
 
 
 CppUnit::Test* TetrisCoreTestSuite::suite()
 {
     CppUnit::TestSuite* pSuite = new CppUnit::TestSuite("TetrisCoreTestSuite");
 
-    pSuite->addTest(NodeTest::suite());
     pSuite->addTest(WorkerTest::suite());
     pSuite->addTest(WorkerPoolTest::suite());
     pSuite->addTest(NodeCalculatorTest::suite());
