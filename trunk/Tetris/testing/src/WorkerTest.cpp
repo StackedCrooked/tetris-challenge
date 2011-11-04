90156e8193ac681275ac425d7582f2587b6b0b33
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-11-04 20:54:24 +0000

    Fix Worker test.

--- trunk/Tetris/testing/src/WorkerTest.cpp
+++ trunk/Tetris/testing/src/WorkerTest.cpp
@@ -1,6 +1,4 @@
 #include "WorkerTest.h"
-#include "CppUnit/TestCaller.h"
-#include "CppUnit/TestSuite.h"
 #include "Futile/Worker.h"
 #include "Futile/Threading.h"
 #include "Futile/Assert.h"
@@ -9,67 +7,39 @@
 #include <iostream>
 
 
-using namespace Futile;
+namespace testing {
 
 
 static const int cSleepTimeMs = 100;
 
 
-WorkerTest::WorkerTest(const std::string & inName):
-    CppUnit::TestCase(inName),
-    mStopwatch(),
+WorkerTest::WorkerTest():
+    TetrisTest(),
     mRepeat(10)
 {
 }
 
 
-WorkerTest::~WorkerTest()
-{
-}
-
-
-void WorkerTest::BeBusy()
-{
-    while (true)
-    {
-        boost::this_thread::interruption_point();
-        Poco::Thread::sleep(cSleepTimeMs);
-    }
-}
-
-
-void WorkerTest::testWorker()
-{
-    std::cout << std::endl;
-    for (size_t idx = 0; idx != mRepeat; ++idx)
-    {
-        std::cout << "\rTest worker iteration " << (idx + 1) << "/" << mRepeat << std::flush;
-        testWorkerImpl();
-    }
-    std::cout << std::endl;
-}
-
-
-void WorkerTest::testWorkerImpl()
+TEST_F(WorkerTest, Worker)
 {
     Worker worker("TestWorker");
-    assertEqual(worker.name(), "TestWorker");
-    Assert(worker.status() >= WorkerStatus_Idle);
+    ASSERT_EQ(worker.name(), "TestWorker");
+    ASSERT_TRUE(worker.status() >= WorkerStatus_Idle);
     worker.wait();
     worker.interrupt();
     worker.interruptAndClearQueue();
-    assertEqual(worker.size(), 0);
+    ASSERT_EQ(worker.size(), 0);
 
 
     // Test without interrupt
     mStopwatch.restart();
     worker.schedule(boost::bind(&Poco::Thread::sleep, cSleepTimeMs));
     worker.waitForStatus(WorkerStatus_Working);
-    assertEqual(worker.size(), 0);
+    ASSERT_EQ(worker.size(), 0);
     worker.wait();
     mStopwatch.stop();
-    Assert(mStopwatch.elapsed() / 1000.0 >= cSleepTimeMs - 100);
-    Assert(mStopwatch.elapsed() / 1000.0 < 100 + cSleepTimeMs);
+    ASSERT_TRUE(mStopwatch.elapsed() / 1000.0 >= cSleepTimeMs - 100);
+    ASSERT_TRUE(mStopwatch.elapsed() / 1000.0 < 100 + cSleepTimeMs);
 
 
     // Test with interrupt
@@ -77,9 +47,9 @@ void WorkerTest::testWorkerImpl()
     worker.schedule(boost::bind(&WorkerTest::BeBusy));
     worker.waitForStatus(WorkerStatus_Working);
     worker.interrupt();
-    assertEqual(worker.size(), 0);
+    ASSERT_EQ(worker.size(), 0);
     mStopwatch.stop();
-    Assert(mStopwatch.elapsed() / 1000.0 < 100 + cSleepTimeMs);
+    ASSERT_TRUE(mStopwatch.elapsed() / 1000.0 < 100 + cSleepTimeMs);
 
 
     // Test with interrupt
@@ -90,17 +60,17 @@ void WorkerTest::testWorkerImpl()
     worker.schedule(boost::bind(&WorkerTest::BeBusy));
     worker.schedule(boost::bind(&WorkerTest::BeBusy));
     worker.waitForStatus(WorkerStatus_Working);
-    assertEqual(worker.size(), 4);
+    ASSERT_EQ(worker.size(), 4);
     worker.interrupt();
-    Assert(mStopwatch.elapsed() / 1000 < 2 * cSleepTimeMs);
-    assertEqual(worker.size(), 3);
+    ASSERT_TRUE(mStopwatch.elapsed() / 1000 < 2 * cSleepTimeMs);
+    ASSERT_EQ(worker.size(), 3);
     worker.interrupt();
-    Assert(mStopwatch.elapsed() / 1000 < 3 * cSleepTimeMs);
-    assertEqual(worker.size(), 2);
+    ASSERT_TRUE(mStopwatch.elapsed() / 1000 < 3 * cSleepTimeMs);
+    ASSERT_EQ(worker.size(), 2);
     worker.interruptAndClearQueue();
-    assertEqual(worker.size(), 0);
+    ASSERT_EQ(worker.size(), 0);
     mStopwatch.stop();
-    Assert(mStopwatch.elapsed() / 1000 < 4 * cSleepTimeMs);
+    ASSERT_TRUE(mStopwatch.elapsed() / 1000 < 4 * cSleepTimeMs);
 
     // Interrupt twice should not crash.
     worker.interrupt();
@@ -110,19 +80,4 @@ void WorkerTest::testWorkerImpl()
 }
 
 
-void WorkerTest::setUp()
-{
-}
-
-
-void WorkerTest::tearDown()
-{
-}
-
-
-CppUnit::Test * WorkerTest::suite()
-{
-    CppUnit::TestSuite * suite(new CppUnit::TestSuite("WorkerTest"));
-    CppUnit_addTest(suite, WorkerTest, testWorker);
-    return suite;
-}
+} // namespace testing
