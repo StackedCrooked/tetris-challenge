941033184a701e130210ec3b18bd0fe09982943a
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-11-05 22:26:37 +0000

    Implement a very simple transaction sample.

--- Tetris/testing/src/STMTest.cpp
+++ Tetris/testing/src/STMTest.cpp
@@ -1,5 +1,6 @@
 #include "TetrisTest.h"
 #include "stm.hpp"
+#include <string>
 
 
 namespace testing {
@@ -8,6 +9,18 @@ namespace testing {
 class STMTest : public TetrisTest
 {
 };
+
+
+TEST_F(STMTest, General)
+{
+    typedef stm::shared<std::string> SharedString;
+    SharedString sharedString(std::string("There"));
+
+    stm::atomic([&](stm::transaction & tx){
+        std::string & str = sharedString.open_rw(tx);
+        str.append("A");
+    });
+}
 
 
 } // namespace testing
