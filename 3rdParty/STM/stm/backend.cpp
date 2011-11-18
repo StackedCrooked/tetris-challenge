c4b662e05f8c23838b549c603936b149574884fd
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-11-18 23:09:26 +0000

    Merge from trunk.

--- 3rdParty/STM/stm/backend.cpp
+++
@@ -1,5 +0,0 @@
-#include "config.hpp"
-
-#if !defined(STM_HEADER_ONLY)
-#include "backend_impl.hpp"
-#endif
--- 3rdParty/STM/tests/abort_test.cpp
+++
@@ -1,7 +0,0 @@
-#include <stm.hpp>
-#include <catch.hpp>
-
-TEST_CASE("abort/onthrow", "Aborts when exception is thrown in transaction")
-{
-	REQUIRE_THROWS(stm::atomic([](stm::transaction&){throw std::exception();}));	
-}
