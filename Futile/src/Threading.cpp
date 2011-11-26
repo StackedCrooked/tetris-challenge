d4ffd739fa77be226c087a86b875f63522e7d8db
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-11-26 02:13:51 +0000

    Introduce (or better, reintroduce) the LifeTimeChecker.

--- Futile/src/Threading.cpp
+++ Futile/src/Threading.cpp
@@ -1,4 +1,7 @@
 #include "Futile/Threading.h"
+#include "Futile/Atomic.h"
+#include "Futile/Logging.h"
+#include "Futile/MakeString.h"
 #include <boost/date_time/posix_time/posix_time_types.hpp>
 #include <ctime>
 
@@ -18,6 +21,40 @@ UInt64 GetCurrentTimeMs()
     boost::posix_time::time_duration duration(time.time_of_day());
     return static_cast<UInt64>(duration.total_milliseconds());
 }
+
+
+namespace {
+
+Atomic<unsigned> gMaxDuration(500);
+
+
+unsigned GetMaxDuration()
+{
+    unsigned result = gMaxDuration.get();
+    if (result % 10 == 0)
+    {
+        LogInfo(SS() << "Max duration is now: " << result << " ms.");
+    }
+    gMaxDuration.decrement(1);
+    return result;
+}
+
+
+}
+
+
+LifeTimeChecker::LifeTimeChecker() :
+	mBeginTime(GetCurrentTimeMs()),
+	mMaxDuration(100)
+{
+}
+
+
+LifeTimeChecker::~LifeTimeChecker()
+{
+	Assert(GetCurrentTimeMs() - mBeginTime <= mMaxDuration);
+}
+
 
 
 } // namespace Futile
