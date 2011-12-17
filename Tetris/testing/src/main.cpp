13dae45a34c5528ad7bb8517c345646d7ccc990e
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2011-12-17 21:38:53 +0000

    Unit tests work now. One test fails.

--- Tetris/testing/src/main.cpp
+++ Tetris/testing/src/main.cpp
@@ -1,12 +1,24 @@
+#include "Futile/Logger.h"
 #include <gtest/gtest.h>
+#include <iostream>
 
 
 using testing::Test;
 using testing::InitGoogleTest;
+using namespace Futile;
+
+
+void Print(const std::string & msg)
+{
+    std::cout << msg << std::endl;
+}
 
 
 int main(int argc, char **argv)
 {
+    Logger::ScopedInitializer initLogger;
+    Logger & logger = Logger::Instance();
+    logger.setLogHandler(Print);
     InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
