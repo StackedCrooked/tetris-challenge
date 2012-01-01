8ff1ba0cb97506bf1d0497d87f67a724b06b6b65
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2012-01-01 09:28:33 +0000

    Valgrind scripts now take  at the end instead of .

--- Scripts/Valgrind/DRD.sh
+++ Scripts/Valgrind/DRD.sh
@@ -1,1 +1,1 @@
-valgrind --tool=drd --check-stack-var=yes --first-race-only=yes --free-is-write=yes 2>valgrind.out $1
+valgrind --tool=drd --check-stack-var=yes --first-race-only=yes --free-is-write=yes 2>valgrind.out $*
