7d85d68c31bb002d8c6c0bd1cd13e3ca4e4cf057
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2012-01-01 09:05:42 +0000

    Fix Tetris/testing target.

--- Tetris/testing/src/ComputerPlayerTest.cpp
+++ Tetris/testing/src/ComputerPlayerTest.cpp
@@ -1,5 +1,6 @@
 #include "TetrisTest.h"
 #include "Tetris/ComputerPlayer.h"
+#include "Tetris/Game.h"
 #include "Futile/Threading.h"
 #include <iostream>
 #include <string>
@@ -18,10 +19,8 @@ class ComputerPlayerTest : public TetrisTest
 
 TEST_F(ComputerPlayerTest, All)
 {
-    PlayerPtr computer = ComputerPlayer::Create(TeamName("AI"),
-                                                PlayerName("Computer0"),
-                                                20,
-                                                10);
+    Game game(20, 10);
+    Computer computer(game);
     std::cout << "Let computer work for 10s" << std::endl;
     Futile::Sleep(10000);
 }
