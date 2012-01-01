8404f36d1d78fa354f2e1720f0b2617fe407417d
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2012-01-01 09:05:42 +0000

    Fix Tetris/testing target.

--- branches/jalf-stm/Tetris/testing/src/main.cpp
+++ branches/jalf-stm/Tetris/testing/src/main.cpp
@@ -1,4 +1,5 @@
 #include "Futile/Logger.h"
+#include "Futile/MainThreadImpl.h"
 #include <gtest/gtest.h>
 #include <iostream>
 
@@ -6,6 +7,18 @@
 using testing::Test;
 using testing::InitGoogleTest;
 using namespace Futile;
+
+
+namespace Futile {
+
+
+std::unique_ptr<MainThreadImpl> CreateMainThreadImpl()
+{
+    throw std::logic_error("Tetris unit tests should not require a message loop.");
+}
+
+
+} // namespace Futile
 
 
 void Print(const std::string & msg)
