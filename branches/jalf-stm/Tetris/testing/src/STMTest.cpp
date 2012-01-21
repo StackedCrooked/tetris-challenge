e7faa2b1c13516dafabed8d4c572a96f03354821
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2012-01-21 20:48:43 +0000

    Implement STMTest.

--- branches/jalf-stm/Tetris/testing/src/STMTest.cpp
+++ branches/jalf-stm/Tetris/testing/src/STMTest.cpp
@@ -1,16 +1,35 @@
 #include "TetrisTest.h"
+#include "Futile/WorkerPool.h"
+#include "Futile/Worker.h"
 #include "stm.hpp"
+#include <atomic>
 #include <string>
 
 
 namespace testing {
 
 
+using namespace Futile;
+
+
 typedef stm::shared<int> SharedInt;
 
 
-struct STMTest : public TetrisTest
+class STMTest : public TetrisTest
 {
+public:
+    STMTest() :
+        stop(false),
+        a(0),
+        b(0),
+        c(0),
+        sum_ab(0),
+        sum_bc(0),
+        sum_ac(0)
+    {
+    }
+
+    std::atomic_bool stop;
     SharedInt a;
     SharedInt b;
     SharedInt c;
@@ -54,19 +73,97 @@ struct STMTest : public TetrisTest
         sum_bc = b.open_r(tx) + c;
     }
 
-    void check(stm::transaction & tx)
+    void check()
     {
-        stm::atomic([&](stm::transaction & tx) { a.open_r(tx) + b.open_r(tx) == sum_ab.open_r(tx); });
-        stm::atomic([&](stm::transaction & tx) { a.open_r(tx) + c.open_r(tx) == sum_ac.open_r(tx); });
-        stm::atomic([&](stm::transaction & tx) { b.open_r(tx) + c.open_r(tx) == sum_bc.open_r(tx); });
+        stm::atomic([&](stm::transaction & tx) {
+            ASSERT_TRUE(a.open_r(tx) + b.open_r(tx) == sum_ab.open_r(tx));
+            ASSERT_TRUE(a.open_r(tx) + c.open_r(tx) == sum_ac.open_r(tx));
+            ASSERT_TRUE(b.open_r(tx) + c.open_r(tx) == sum_bc.open_r(tx));
+        });
     }
 
+    void test()
+    {
+        while (!stop)
+        {
+            stm::atomic([&](stm::transaction & tx) {
+                increment_a(tx);
+            });
+            check();
+
+            stm::atomic([&](stm::transaction & tx) {
+                increment_b(tx);
+            });
+            check();
+
+            stm::atomic([&](stm::transaction & tx) {
+                increment_c(tx);
+            });
+            check();
+
+            stm::atomic([&](stm::transaction & tx) {
+                increment_a(tx);
+                increment_b(tx);
+            });
+            check();
+
+            stm::atomic([&](stm::transaction & tx) {
+                increment_b(tx);
+                increment_c(tx);
+            });
+            check();
+
+            stm::atomic([&](stm::transaction & tx) {
+                increment_a(tx);
+                increment_c(tx);
+            });
+            check();
+
+            stm::atomic([&](stm::transaction & tx) {
+                increment_a(tx);
+                increment_b(tx);
+                increment_c(tx);
+            });
+            check();
+        }
+    }
+
+    void printValues()
+    {
+        stm::atomic([&](stm::transaction & tx) {
+            std::cout << "a: " << a.open_r(tx) << std::endl;
+            std::cout << "b: " << b.open_r(tx) << std::endl;
+            std::cout << "c: " << c.open_r(tx) << std::endl;
+            std::cout << "sum_ab: " << sum_ab.open_r(tx) << std::endl;
+            std::cout << "sum_ac: " << sum_ac.open_r(tx) << std::endl;
+            std::cout << "sum_bc: " << sum_bc.open_r(tx) << std::endl;
+        });
+    }
 };
 
 
 TEST_F(STMTest, CoordinatedChanges)
 {
 
+    WorkerPool workers("Incrementers", 16);
+    for (std::size_t idx = 0; idx < workers.size(); ++idx)
+    {
+        workers.schedule(boost::bind(&STMTest::test, this));
+    }
+
+    static const unsigned cDuration = 5000;
+
+    std::cout << "Wait for " << cDuration << "ms..." << std::endl;
+    Sleep(cDuration);
+
+    std::cout << "Stopping the workers..." << std::endl;
+    stop = true;
+    workers.wait();
+
+    std::cout << "Results:" << std::endl;
+    printValues();
+
+    std::cout << "Finished." << std::endl;
 }
 
 
