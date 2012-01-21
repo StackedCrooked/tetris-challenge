713e726e39ad9afb9c64964021c1ca893f0ca52e
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2012-01-21 20:18:05 +0000

    Update STMTest.cpp.

--- Tetris/testing/src/STMTest.cpp
+++ Tetris/testing/src/STMTest.cpp
@@ -6,20 +6,67 @@
 namespace testing {
 
 
-class STMTest : public TetrisTest
+typedef stm::shared<int> SharedInt;
+
+
+struct STMTest : public TetrisTest
 {
+    SharedInt a;
+    SharedInt b;
+    SharedInt c;
+    SharedInt sum_ab;
+    SharedInt sum_bc;
+    SharedInt sum_ac;
+
+    void increment_a(stm::transaction & tx)
+    {
+        int & a = this->a.open_rw(tx);
+        a++;
+
+        int & sum_ab = this->sum_ab.open_rw(tx);
+        sum_ab = a + b.open_r(tx);
+
+        int & sum_ac = this->sum_ac.open_rw(tx);
+        sum_ac = a + c.open_r(tx);
+    }
+
+    void increment_b(stm::transaction & tx)
+    {
+        int & b = this->b.open_rw(tx);
+        b++;
+
+        int & sum_ab = this->sum_ab.open_rw(tx);
+        sum_ab = a.open_r(tx) + b;
+
+        int & sum_bc = this->sum_bc.open_rw(tx);
+        sum_bc = b + c.open_r(tx);
+    }
+
+    void increment_c(stm::transaction & tx)
+    {
+        int & c = this->c.open_rw(tx);
+        c++;
+
+        int & sum_ac = this->sum_ac.open_rw(tx);
+        sum_ac = a.open_r(tx) + c;
+
+        int & sum_bc = this->sum_bc.open_rw(tx);
+        sum_bc = b.open_r(tx) + c;
+    }
+
+    void check(stm::transaction & tx)
+    {
+        stm::atomic([&](stm::transaction & tx) { a.open_r(tx) + b.open_r(tx) == sum_ab.open_r(tx); });
+        stm::atomic([&](stm::transaction & tx) { a.open_r(tx) + c.open_r(tx) == sum_ac.open_r(tx); });
+        stm::atomic([&](stm::transaction & tx) { b.open_r(tx) + c.open_r(tx) == sum_bc.open_r(tx); });
+    }
+
 };
 
 
-TEST_F(STMTest, General)
+TEST_F(STMTest, CoordinatedChanges)
 {
-    typedef stm::shared<std::string> SharedString;
-    SharedString sharedString(std::string("There"));
 
-    stm::atomic([&](stm::transaction & tx){
-        std::string & str = sharedString.open_rw(tx);
-        str.append("A");
-    });
 }
 
 
