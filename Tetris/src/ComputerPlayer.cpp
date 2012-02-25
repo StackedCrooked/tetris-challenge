b61d6a6187b3a8aece05d3202544235b13e1197d
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2012-02-25 04:53:22 +0000

    Wrap non-stm resources in sep struct.

--- Tetris/src/ComputerPlayer.cpp
+++ Tetris/src/ComputerPlayer.cpp
@@ -48,6 +48,21 @@ static const unsigned cDefaultWorkerCount = 8;
 namespace {
 
 
+struct ExclusiveResources
+{
+    ExclusiveResources() :
+        mMainWorker("Computer main worker"),
+        mWorkerPool("Computer worker pool", cDefaultWorkerCount),
+        mNodeCalculator()
+    {
+    }
+
+    Worker mMainWorker;
+    WorkerPool mWorkerPool;
+    boost::scoped_ptr<NodeCalculator> mNodeCalculator;
+};
+
+
 typedef std::vector<GameState> Precalculated;
 
 
@@ -77,31 +92,32 @@ struct NodeCalculatorParams
 struct Computer::Impl : boost::noncopyable
 {
 public:
-    Impl(Computer * inComputer, Game & inGame) :
-        mComputer(inComputer),
-        mGame(inGame),
-        mMainWorker("Computer: MainWorker"),
-        mWorkerPool("Computer: WorkerPool", cDefaultWorkerCount),
-        mPrecalculated(Precalculated()),
-        mNodeCalculator(),
+    Impl(Game & inGame) :
+        mExclusiveResources(new ExclusiveResources),
         mEvaluator(&MakeTetrises::Instance()),
+        mGame(inGame),
+        mPrecalculated(Precalculated()),
         mSearchDepth(8),
         mSearchWidth(5),
-        mWorkerCount(cDefaultWorkerCount),
-        mQuitFlag(false)
+        mWorkerCount(cDefaultWorkerCount)
     {
     }
 
     ~Impl()
     {
-        if (mNodeCalculator)
+        FUTILE_LOCK(ExclusiveResources & res, mExclusiveResources)
         {
-            mNodeCalculator->stop();
+            if (res.mNodeCalculator)
+            {
+                res.mNodeCalculator->stop();
+            }
+            res.mWorkerPool.interruptAndClearQueue();
         }
-        mWorkerPool.interruptAndClearQueue();
     }
 
     void checkNodeCalculator();
+
+    void checkNodeCalculator(ExclusiveResources & res);
 
     Precalculated getPrecalculatedCopy() const
     {
@@ -137,21 +153,12 @@ public:
 
     void move(stm::transaction & tx);
 
-    static void UpdateComputerBlockMoveSpeed(boost::weak_ptr<Player> weakPlayer) {
-        if (PlayerPtr playerPtr = weakPlayer.lock())
-        {
-            Computer & cp = dynamic_cast<Computer&>(*playerPtr);
-            cp.mImpl.lock()->updateComputerBlockMoveSpeed();
-        }
-    }
-
-    void updateComputerBlockMoveSpeed();
-
     boost::optional<NodeCalculatorParams> getNodeCalculatorParams();
 
     boost::optional<NodeCalculatorParams> getNodeCalculatorParams(stm::transaction & tx);
 
-    void startNodeCalculator(const GameState & inGameState,
+    void startNodeCalculator(ExclusiveResources & res,
+                             const GameState & inGameState,
                              const std::vector<BlockType> & inBlockTypes,
                              const std::vector<int> & inWidths,
                              unsigned inWorkerCount);
@@ -179,23 +186,21 @@ public:
                                       : cPrecalculated.back().originalBlock();
     }
 
-    Computer * mComputer;
+    // Can't use STM on this
+    ThreadSafe<ExclusiveResources> mExclusiveResources;
+
+    const Evaluator * mEvaluator;
     Game & mGame;
 
-    Worker mMainWorker;
-    WorkerPool mWorkerPool;
     mutable stm::shared<Precalculated> mPrecalculated;
-    boost::scoped_ptr<NodeCalculator> mNodeCalculator;
-    const Evaluator * mEvaluator;
-    int mSearchDepth;
-    int mSearchWidth;
-    int mWorkerCount;
-    bool mQuitFlag;
+    mutable stm::shared<int> mSearchDepth;
+    mutable stm::shared<int> mSearchWidth;
+    mutable stm::shared<int> mWorkerCount;
 };
 
 
 Computer::Computer(Game & inGame) :
-    mImpl(new Impl(this, inGame)),
+    mImpl(new Impl(inGame)),
     mTimer(new Futile::Timer(100)),
     mMoveTimer(new Futile::Timer(60))
 {
@@ -208,10 +213,6 @@ Computer::~Computer()
 {
     mTimer->stop();
     mMoveTimer->stop();
-    FUTILE_LOCK(Impl & impl, mImpl)
-    {
-        impl.mQuitFlag = true;
-    }
 }
 
 
@@ -219,18 +220,20 @@ void Computer::onTimerEvent()
 {
     try
     {
-        FUTILE_LOCK(Impl & impl, mImpl)
-        {
-            if (!impl.mQuitFlag)
-            {
-                impl.timerEvent();
-            }
-        }
+        mImpl->timerEvent();
     }
     catch (const std::exception & exc)
     {
         LogError(SS() << "Exception caught during Computer timerEvent. Details: " << exc.what());
     }
+}
+
+
+void Computer::Impl::timerEvent()
+{
+    checkForSyncErrors();
+    timerEventImpl();
+    checkForSyncErrors();
 }
 
 
@@ -238,13 +241,7 @@ void Computer::onMoveTimerEvent()
 {
     try
     {
-        FUTILE_LOCK(Impl & impl, mImpl)
-        {
-            if (!impl.mQuitFlag)
-            {
-                impl.moveTimerEvent();
-            }
-        }
+        mImpl->moveTimerEvent();
     }
     catch (const std::exception & exc)
     {
@@ -253,46 +250,49 @@ void Computer::onMoveTimerEvent()
 }
 
 
+void Computer::Impl::moveTimerEvent()
+{
+    checkForSyncErrors();
+    moveTimerEventImpl();
+    checkForSyncErrors();
+}
+
+
 int Computer::searchDepth() const
 {
-    return mImpl.lock()->mSearchDepth;
+    return stm::atomic<int>([&](stm::transaction & tx){
+        return mImpl->mSearchDepth.open_r(tx);
+    });
 }
 
 
 void Computer::setSearchDepth(int inSearchDepth)
 {
-    FUTILE_LOCK(Impl & impl, mImpl)
-    {
-        impl.mSearchDepth = inSearchDepth;
-    }
+    stm::atomic([&](stm::transaction & tx){
+        mImpl->mSearchDepth.open_rw(tx) = inSearchDepth;
+    });
 }
 
 
 int Computer::searchWidth() const
 {
-    return mImpl.lock()->mSearchWidth;
+    return stm::atomic<int>([&](stm::transaction & tx){
+        return mImpl->mSearchWidth.open_r(tx);
+    });
 }
 
 
 void Computer::setSearchWidth(int inSearchWidth)
 {
-    FUTILE_LOCK(Impl & impl, mImpl)
-    {
-        impl.mSearchWidth = inSearchWidth;
-    }
+    stm::atomic([&](stm::transaction & tx){
+        mImpl->mSearchDepth.open_rw(tx) = inSearchWidth;
+    });
 }
 
 
 int Computer::depth() const
 {
-    FUTILE_LOCK(Impl & impl, mImpl)
-    {
-        if (impl.mNodeCalculator)
-        {
-            return impl.mNodeCalculator->getCurrentSearchDepth();
-        }
-    }
-    return 0;
+    return mImpl->mExclusiveResources.lock()->mNodeCalculator->getCurrentSearchDepth();
 }
 
 
@@ -320,23 +320,17 @@ void Computer::setMoveSpeed(int inNumMovesPerSecond)
 
 int Computer::workerCount() const
 {
-    return mImpl.lock()->mWorkerCount;
+    return stm::atomic<int>([&](stm::transaction & tx){
+        return mImpl->mWorkerCount.open_r(tx);
+    });
 }
 
 
 void Computer::setWorkerCount(int inWorkerCount)
 {
-    FUTILE_LOCK(Impl & impl, mImpl)
-    {
-        if (inWorkerCount == 0)
-        {
-            impl.mWorkerCount = cDefaultWorkerCount;
-        }
-        else
-        {
-            impl.mWorkerCount = inWorkerCount;
-        }
-    }
+    stm::atomic([&](stm::transaction & tx){
+        mImpl->mWorkerCount.open_rw(tx) = inWorkerCount;
+    });
 }
 
 
@@ -444,25 +438,10 @@ void Computer::Impl::move(stm::transaction & tx)
 }
 
 
-void Computer::Impl::timerEvent()
-{
-    checkForSyncErrors();
-    timerEventImpl();
-    checkForSyncErrors();
-}
-
-
-void Computer::Impl::moveTimerEvent()
-{
-    checkForSyncErrors();
-    moveTimerEventImpl();
-    checkForSyncErrors();
-}
-
-
 void Computer::Impl::moveTimerEventImpl()
 {
-    stm::atomic([&](stm::transaction & tx) {
+    stm::atomic([&](stm::transaction & tx)
+    {
         const Precalculated & precalculated = mPrecalculated.open_r(tx);
         if (!precalculated.empty())
         {
@@ -480,12 +459,22 @@ void Computer::Impl::timerEventImpl()
 
 void Computer::Impl::checkNodeCalculator()
 {
-    if (!mNodeCalculator)
+    FUTILE_LOCK(ExclusiveResources & res, mExclusiveResources)
+    {
+        checkNodeCalculator(res);
+    }
+}
+
+
+void Computer::Impl::checkNodeCalculator(ExclusiveResources & res)
+{
+    if (!res.mNodeCalculator)
     {
         boost::optional<NodeCalculatorParams> params = getNodeCalculatorParams();
         if (params.is_initialized())
         {
-            startNodeCalculator(params->mGameState,
+            startNodeCalculator(res,
+                                params->mGameState,
                                 params->mBlockTypes,
                                 params->mWidths,
                                 params->mWorkerCount);
@@ -493,7 +482,7 @@ void Computer::Impl::checkNodeCalculator()
         return;
     }
 
-    switch (mNodeCalculator->status())
+    switch (res.mNodeCalculator->status())
     {
         case NodeCalculator::Status_Nil:
         {
@@ -509,20 +498,21 @@ void Computer::Impl::checkNodeCalculator()
             static const unsigned cMinimumPrecalculated = 4;
             if (cPrecalculated.size() <= cMinimumPrecalculated)
             {
-                mNodeCalculator->stop();
-                NodeCalculator::Status status = mNodeCalculator->status();
+                res.mNodeCalculator->stop();
+                NodeCalculator::Status status = res.mNodeCalculator->status();
                 Assert(status == NodeCalculator::Status_Stopped ||
                        status == NodeCalculator::Status_Finished);
                 (void)status;
-                processResults(mNodeCalculator->result());
-                mNodeCalculator.reset();
+                processResults(res.mNodeCalculator->result());
+                res.mNodeCalculator.reset();
                 boost::optional<NodeCalculatorParams> params = getNodeCalculatorParams();
                 if (params.is_initialized())
                 {
-                    startNodeCalculator(params->mGameState,
+                    startNodeCalculator(res,
+                                        params->mGameState,
                                         params->mBlockTypes,
                                         params->mWidths,
-                                        mWorkerPool.size());
+                                        res.mWorkerPool.size());
                 }
             }
             return;
@@ -534,24 +524,24 @@ void Computer::Impl::checkNodeCalculator()
         }
         case NodeCalculator::Status_Finished:
         {
-            processResults(mNodeCalculator->result());
-            mNodeCalculator.reset();
+            processResults(res.mNodeCalculator->result());
+            res.mNodeCalculator.reset();
             boost::optional<NodeCalculatorParams> params = getNodeCalculatorParams();
             if (params.is_initialized())
             {
-                mNodeCalculator.reset(new NodeCalculator(params->mGameState,
-                                                         params->mBlockTypes,
-                                                         params->mWidths,
-                                                         *mEvaluator,
-                                                         mMainWorker,
-                                                         mWorkerPool));
+                res.mNodeCalculator.reset(new NodeCalculator(params->mGameState,
+                                                             params->mBlockTypes,
+                                                             params->mWidths,
+                                                             *mEvaluator,
+                                                             res.mMainWorker,
+                                                             res.mWorkerPool));
             }
             return;
         }
         case NodeCalculator::Status_Error:
         {
-            LogError("Computer: " + mNodeCalculator->errorMessage());
-            mNodeCalculator.reset();
+            LogError("Computer: " + res.mNodeCalculator->errorMessage());
+            res.mNodeCalculator.reset();
             return;
         }
         default:
@@ -562,19 +552,20 @@ void Computer::Impl::checkNodeCalculator()
 }
 
 
-void Computer::Impl::startNodeCalculator(const GameState & inGameState,
+void Computer::Impl::startNodeCalculator(ExclusiveResources & res,
+                                         const GameState & inGameState,
                                          const std::vector<BlockType> & inBlockTypes,
                                          const std::vector<int> & inWidths,
                                          unsigned inWorkerCount)
 {
-    mWorkerPool.resize(inWorkerCount);
-    mNodeCalculator.reset(new NodeCalculator(inGameState,
-                                             inBlockTypes,
-                                             inWidths,
-                                             *mEvaluator,
-                                             mMainWorker,
-                                             mWorkerPool));
-    mNodeCalculator->start();
+    res.mWorkerPool.resize(inWorkerCount);
+    res.mNodeCalculator.reset(new NodeCalculator(inGameState,
+                                                 inBlockTypes,
+                                                 inWidths,
+                                                 *mEvaluator,
+                                                 res.mMainWorker,
+                                                 res.mWorkerPool));
+    res.mNodeCalculator->start();
 }
 
 
@@ -600,7 +591,7 @@ boost::optional<NodeCalculatorParams> Computer::Impl::getNodeCalculatorParams(st
     // Create the list of future blocks
     //
     const Precalculated & cPrecalculated = mPrecalculated.open_r(tx);
-    std::vector<BlockType> nextBlocks = mGame.getFutureBlocks(tx, cPrecalculated.size() + mSearchDepth + 1);
+    std::vector<BlockType> nextBlocks = mGame.getFutureBlocks(tx, cPrecalculated.size() + mSearchDepth.open_r(tx) + 1);
     BlockTypes futureBlocks;
     for (std::size_t idx = cPrecalculated.size(); idx < nextBlocks.size(); ++idx)
     {
@@ -614,7 +605,7 @@ boost::optional<NodeCalculatorParams> Computer::Impl::getNodeCalculatorParams(st
     std::vector<int> widths;
     for (std::size_t idx = 0; idx != futureBlocks.size(); ++idx)
     {
-        widths.push_back(mSearchWidth);
+        widths.push_back(mSearchWidth.open_r(tx));
     }
 
     return Ret(NodeCalculatorParams(previousGameState(tx), futureBlocks, widths, cDefaultWorkerCount));
