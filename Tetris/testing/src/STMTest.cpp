1b3939c4adbd5c1a02de12b6949100e2815bc0a2
francis.rammeloo <francis.rammeloo@773f1350-5858-11de-8888-c79ed7c8b0c3>
2012-04-20 23:11:39 +0000

    Fix STMTest. Can now reproduce issue, at least in Debug mode.

--- Tetris/testing/src/STMTest.cpp
+++ Tetris/testing/src/STMTest.cpp
@@ -8,73 +8,71 @@
 #include <vector>
 #include <unistd.h>
 
-
-typedef stm::shared<int> shared_int;
-
-
 // Global stop flag.
 std::atomic_bool gStop(false);
 
+typedef stm::shared<int> shared_int;
 
-struct STMTest : std::thread
+struct Globals
 {
-    STMTest() :
-        std::thread(std::bind(&STMTest::test, this)),
-        a(0),
-        b(0),
-        c(0),
-        sum_ab(0),
-        sum_bc(0),
-        sum_ac(0)
-    {
-    }
-
     shared_int a;
     shared_int b;
     shared_int c;
     shared_int sum_ab;
     shared_int sum_bc;
     shared_int sum_ac;
+};
+
+
+Globals globals;
+
+
+struct STMTest : std::thread
+{
+    STMTest() :
+        std::thread(std::bind(&STMTest::test, this))
+    {
+    }
 
     void increment_a()
     {
-        stm::atomic([this](stm::transaction& tx) {
-        int & a = this->a.open_rw(tx);
+        stm::atomic([&](stm::transaction& tx) {
+        int & a = globals.a.open_rw(tx);
         a++;
 
-        int & sum_ab = this->sum_ab.open_rw(tx);
-        sum_ab = a + b.open_r(tx);
+        int & sum_ab = globals.sum_ab.open_rw(tx);
+        sum_ab = a + globals.b.open_r(tx);
 
-        int & sum_ac = this->sum_ac.open_rw(tx);
-        sum_ac = a + c.open_r(tx);
+        int & sum_ac = globals.sum_ac.open_rw(tx);
+        sum_ac = a + globals.c.open_r(tx);
         });
     }
 
     void increment_b()
     {
-        stm::atomic([this](stm::transaction& tx) {
-        int & b = this->b.open_rw(tx);
+        stm::atomic([&](stm::transaction& tx) {
+        int & b = globals.b.open_rw(tx);
         b++;
 
-        int & sum_ab = this->sum_ab.open_rw(tx);
-        sum_ab = a.open_r(tx) + b;
+        int & sum_ab = globals.sum_ab.open_rw(tx);
+        sum_ab = globals.a.open_r(tx) + b;
 
-        int & sum_bc = this->sum_bc.open_rw(tx);
-        sum_bc = b + c.open_r(tx);
+        int & sum_bc = globals.sum_bc.open_rw(tx);
+        sum_bc = b + globals.c.open_r(tx);
         });
     }
 
     void increment_c()
     {
-        stm::atomic([this](stm::transaction& tx) {
-        int & c = this->c.open_rw(tx);
+        stm::atomic([&](stm::transaction& tx) {
+        int & c = globals.c.open_rw(tx);
         c++;
 
-        int & sum_ac = this->sum_ac.open_rw(tx);
-        sum_ac = a.open_r(tx) + c;
+        int & sum_ac = globals.sum_ac.open_rw(tx);
+        sum_ac = globals.a.open_r(tx) + c;
 
-        int & sum_bc = this->sum_bc.open_rw(tx);
-        sum_bc = b.open_r(tx) + c;
+        int & sum_bc = globals.sum_bc.open_rw(tx);
+        sum_bc = globals.b.open_r(tx) + c;
         });
     }
 
@@ -89,9 +87,9 @@ struct STMTest : std::thread
     void check()
     {
         stm::atomic([&](stm::transaction & tx) {
-            assertTrue(a.open_r(tx) + b.open_r(tx) == sum_ab.open_r(tx));
-            assertTrue(a.open_r(tx) + c.open_r(tx) == sum_ac.open_r(tx));
-            assertTrue(b.open_r(tx) + c.open_r(tx) == sum_bc.open_r(tx));
+            assertTrue(globals.a.open_r(tx) + globals.b.open_r(tx) == globals.sum_ab.open_r(tx));
+            assertTrue(globals.a.open_r(tx) + globals.c.open_r(tx) == globals.sum_ac.open_r(tx));
+            assertTrue(globals.b.open_r(tx) + globals.c.open_r(tx) == globals.sum_bc.open_r(tx));
         });
     }
 
@@ -140,39 +138,43 @@ struct STMTest : std::thread
             check();
         }
     }
-
-    typedef std::vector<int> Values;
-    Values getValues(stm::transaction & tx)
-    {
-        Values results;
-        results.push_back(a.open_r(tx));
-        results.push_back(b.open_r(tx));
-        results.push_back(c.open_r(tx));
-        results.push_back(sum_ab.open_r(tx));
-        results.push_back(sum_ac.open_r(tx));
-        results.push_back(sum_bc.open_r(tx));
-        return results;
-    }
-
-    void printValues()
-    {
-        Values values = stm::atomic<Values>([&](stm::transaction & tx) { return this->getValues(tx); });
-        for (Values::size_type idx = 0; idx < values.size(); ++idx)
-        {
-            if (idx != 0)
-            {
-                std::cout << ", ";
-            }
-
-            if (idx % 6 == 0)
-            {
-                std::cout << std::endl;
-            }
-
-            std::cout << values[idx];
-        }
-    }
 };
+
+
+typedef std::vector<int> Values;
+
+
+Values getValues(stm::transaction & tx)
+{
+    Values results;
+    results.push_back(globals.a.open_r(tx));
+    results.push_back(globals.b.open_r(tx));
+    results.push_back(globals.c.open_r(tx));
+    results.push_back(globals.sum_ab.open_r(tx));
+    results.push_back(globals.sum_ac.open_r(tx));
+    results.push_back(globals.sum_bc.open_r(tx));
+    return results;
+}
+
+
+void printValues()
+{
+    Values values = stm::atomic<Values>([&](stm::transaction & tx) { return getValues(tx); });
+    for (Values::size_type idx = 0; idx < values.size(); ++idx)
+    {
+        if (idx != 0)
+        {
+            std::cout << ", ";
+        }
+
+        if (idx % 6 == 0)
+        {
+            std::cout << std::endl;
+        }
+
+        std::cout << values[idx];
+    }
+}
 
 
 void test()
@@ -191,10 +193,10 @@ void test()
     for (auto & worker : workers)
     {
         worker.join();
-        worker.printValues();
     }
-    std::cout << std::endl;
 
+    printValues();
+    std::cout << std::endl;
     std::cout << "Finished." << std::endl;
 }
 
