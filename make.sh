9b1dfcd125306cfba3dd14b8d9acdb2adf46fae2
StackedCrooked <francis.rammeloo@gmail.com>
2019-10-11 09:20:38 +0200

    Cleanup make.sh.

--- make.sh
+++ make.sh
@@ -8,16 +8,5 @@ set -e
 cd Build
 make -j7
 
-# Note
-# ----
-# The produced binary needs to know where to find the Qt libs. We tell it to
-# look for the frameworks in the Qt installation directory where we found "moc".
-{ otool -L bin/QtTetris | grep 'rpath'; } >/dev/null || {
-    QT_LIB_DIR="$(dirname "$(dirname $(which moc))")/lib"
-    install_name_tool -add_rpath $QT_LIB_DIR bin/QtTetris
-}
-
-
 # Run the unit tests 
-./bin/TetrisTest
-
+Tetris/testing/TetrisTest
